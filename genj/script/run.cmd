090d13d1104d42a8075a79278d75d962519efea1
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2002-03-07 23:20:30 +0000

    1st
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@5 fbd980a2-0dff-11df-b4b7-0b05b74d0985

---
+++ genj/script/run.cmd
@@ -0,0 +1,20 @@
+@echo off
+
+echo /**
+echo  * GenJ - Start Script
+echo  * 
+echo  * If something goes wrong now you probably have an
+echo  * incomplete CLASSPATH. Either copy swing.jar to
+echo  * the ./lib directory or specify its location via
+echo  *   set CLASSPATH=c:\swing\swing.jar
+echo  * before starting this script.
+echo  *
+echo  */
+
+rem switch off JIT by uncommenting next line
+rem set JAVA_COMPILER=none
+
+set LANG=./lib/genj_de.zip;./lib/genj_en.zip;./lib/genj_fr.zip
+set CLASSPATH=./lib/genj.jar;./lib/swing.jar;./lib/jhbasic.jar;%LANG%;%CLASSPATH%
+java genj.app.App
+
