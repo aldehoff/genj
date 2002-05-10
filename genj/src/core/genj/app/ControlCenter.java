20607e2bba91583ca95465b84525a534c9eb7a2c
island1 <island1@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2002-05-10 20:10:44 +0000

    add a new dialog menu
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@132 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/src/core/genj/app/ControlCenter.java
+++ genj/src/core/genj/app/ControlCenter.java
@@ -512,10 +512,12 @@ public class ControlCenter extends JPanel implements ActionListener {
 
     // ABOUT?
     if (e.getActionCommand().equals("ABOUT")) {
-      JOptionPane.showMessageDialog(this,
-      App.resources.getString("app.title")+" "+Version.getInstance().toString(),
-      App.resources.getString("cc.menu.about"),
-      JOptionPane.INFORMATION_MESSAGE);
+// F. Massonneau ---------- 05-08-2002
+      AboutMenu.showDialogMenu();
+//      JOptionPane.showMessageDialog(this,
+//      App.resources.getString("app.title")+" "+Version.getInstance().toString(),
+//      App.resources.getString("cc.menu.about"),
+//      JOptionPane.INFORMATION_MESSAGE);
       return;
 
     }
