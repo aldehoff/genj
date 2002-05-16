b1b17b9bf99acc04d36d69c3df398e4466e0df76
island1 <island1@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2002-05-16 21:04:03 +0000

    Copyright Panel updated
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@143 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/src/core/genj/app/CopyrightPanel.java
+++ genj/src/core/genj/app/CopyrightPanel.java
@@ -20,7 +20,7 @@
  * About Menu class - Copyright
  * This class creates the content on the Copyright tabbed pane in the
  * About Menu application
- * $Header: /cygdrive/c/temp/cvs/genj/genj/src/core/genj/app/CopyrightPanel.java,v 1.2 2002-05-11 06:27:30 island1 Exp $
+ * $Header: /cygdrive/c/temp/cvs/genj/genj/src/core/genj/app/CopyrightPanel.java,v 1.3 2002-05-16 21:04:03 island1 Exp $
  * @author Francois Massonneau <frmas@free.fr>
  * @version 1.0
  *
@@ -31,20 +31,90 @@ package genj.app;
 
 // Import for buttons, labels, and images
 import javax.swing.*;
+import javax.swing.border.*;
 // Import for layout manager.
 import java.awt.*;
+import java.io.*;
+
 
 public class CopyrightPanel extends JPanel
 { // Opens class
-
+  
+  // Some variables
+  private JPanel panelCopyR;
+  private JPanel panelGpl;
+  private JTextArea taCopyR;
+  private JTextArea taGpl;
+  
     public CopyrightPanel()
   {  // Opens constructor
     // Sets the layout by instantiating a BorderLayout container in the setLayout method
     setLayout(new BorderLayout());
     // Sets the background color for this copyright panel object
-    setBackground(Color.white);
+//    setBackground(Color.white);
+    
+    // Call method to build Panel 1 - panelCopyR
+    buildPanelCopyr();
+    
+    // Call method to build Panel 2 - panelGpl
+    buildPanelGpl();
+    
+    // Time to add each panel to the main panel
+    add(panelCopyR, BorderLayout.NORTH);
+    add(panelGpl, BorderLayout.CENTER);
     
   }  // Closes constructor
-
+  
+  // This method create the panel to display the Copyright stuff
+  private void buildPanelCopyr() {
+    panelCopyR = new JPanel();
+    panelCopyR.setLayout(new BorderLayout());
+    panelCopyR.setBackground(Color.white);
+    panelCopyR.setBorder(BorderFactory.createTitledBorder(App.resources.getString("cc.about.tab4.text1.title")));
+    taCopyR = new JTextArea(App.resources.getString("cc.about.tab4.text1"));
+    // Sets the font face and size
+    taCopyR.setFont(new Font("Times-Roman", Font.PLAIN, 12));
+    // Line wrap is set for the text area and it is not editable
+    taCopyR.setLineWrap(true);
+    taCopyR.setWrapStyleWord(true);
+    taCopyR.setEditable(false);
+    taCopyR.setBorder(new EmptyBorder(5, 5, 5, 5));
+    panelCopyR.add(taCopyR, BorderLayout.CENTER);
+  }
+    
+  // This method create the panel to display the GPL file
+  private void buildPanelGpl() {
+    panelGpl = new JPanel();
+    panelGpl.setLayout(new BorderLayout());
+    panelGpl.setBackground(Color.white);
+    panelGpl.setBorder(BorderFactory.createTitledBorder(App.resources.getString("cc.about.tab4.text2.title")));
+    taGpl = new JTextArea();
+    readTextFile(taGpl, "./doc/gpl.txt", App.resources.getString("cc.about.tab4.text2"));
+    taGpl.setFont(new Font("Times-Roman", Font.PLAIN, 12));
+    taGpl.setLineWrap(true);
+    taGpl.setWrapStyleWord(true);
+    taGpl.setEditable(false);
+    taGpl.setBorder(new EmptyBorder(5, 5, 5, 5));
+    JScrollPane spGpl = new JScrollPane(taGpl);
+    spGpl.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+    spGpl.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+    panelGpl.add(spGpl, BorderLayout.CENTER);
+  }
+  
+  // This method reads the text to feed the variable taGpl and if the text file is
+  // missing, it sends a substitute sentence.
+  private void readTextFile(JTextArea ta, String file, String fallback) {
+    try {
+      FileInputStream fin = new FileInputStream(file);
+      Reader in = new InputStreamReader(fin);
+      ta.read(in,null);
+      fin.close();
+    }
+    catch (Exception e) {
+      ta.setText(fallback);
+    }
+  }
+  
+  
 } // Closes class
 
