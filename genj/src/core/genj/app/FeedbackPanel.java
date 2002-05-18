ba648ce24ccac39bcc5d57cec9aefd57613648c6
island1 <island1@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2002-05-18 07:32:14 +0000

    code added to get system info
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@149 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/src/core/genj/app/FeedbackPanel.java
+++ genj/src/core/genj/app/FeedbackPanel.java
@@ -20,9 +20,9 @@
  * About Menu class - Feedback
  * This class creates the content on the Feedback tabbed pane in the
  * About Menu application
- * $Header: /cygdrive/c/temp/cvs/genj/genj/src/core/genj/app/FeedbackPanel.java,v 1.2 2002-05-16 21:30:34 island1 Exp $
+ * $Header: /cygdrive/c/temp/cvs/genj/genj/src/core/genj/app/FeedbackPanel.java,v 1.3 2002-05-18 07:32:14 island1 Exp $
  * @author Francois Massonneau <frmas@free.fr>
- * @version 1.0
+ * @version 1.1
  *
  */
 
@@ -34,6 +34,10 @@ import javax.swing.border.*;
 // Import for layout manager.
 import java.awt.*;
 import java.io.*;
+import java.util.*;
+
+import genj.Version;
+
 
 public class FeedbackPanel extends JPanel
 { // Opens class
@@ -87,7 +91,7 @@ public class FeedbackPanel extends JPanel
     ta1Panel1.setLineWrap(true);
     ta1Panel1.setWrapStyleWord(true);
     ta1Panel1.setEditable(false);
-    ta1Panel1.setBorder(new EmptyBorder(5, 5, 5, 5));
+    ta1Panel1.setBorder(new EmptyBorder(3, 3, 3, 3));
     panelUp.add(ta1Panel1, BorderLayout.CENTER);
   }
   
@@ -100,6 +104,7 @@ public class FeedbackPanel extends JPanel
     ta1Panel2.setLineWrap(true);
     ta1Panel2.setWrapStyleWord(true);
     ta1Panel2.setEditable(true);
+    ta1Panel2.setBorder(new EmptyBorder(3, 3, 3, 3));
     JScrollPane jspta1 = new JScrollPane(ta1Panel2);
     panelMiddle.add(jspta1, BorderLayout.CENTER);
   }
@@ -114,9 +119,34 @@ public class FeedbackPanel extends JPanel
   }
   
     // This method gets the User's system info
-  public static String getSystemInfo() {
+  public static String getSystemInfo()
+  {
+   	Properties p = System.getProperties();
     StringBuffer sb = new StringBuffer();
-   	sb.append("Not Yet Implemented");
+   	sb.append(App.resources.getString("cc.about.tab3.text2.1"));
+    sb.append(": \n");
+    sb.append(p.get("os.name"));
+    sb.append(" ");
+    sb.append(p.get("os.version"));
+    sb.append(" (");
+    sb.append(p.get("os.arch"));
+    sb.append(") - JRE: ");
+    sb.append(p.get("java.vendor"));
+    sb.append(" ");
+    sb.append(p.get("java.version"));
+    sb.append(" - ");
+    sb.append("Class version: ");
+    sb.append(p.get("java.class.version"));
+    sb.append("\n" + App.resources.getString("cc.about.tab3.text2.2"));
+    sb.append(" " + App.resources.getString("app.title")+" "+Version.getInstance().toString());
+    sb.append(".\n\n");
+    sb.append(App.resources.getString("cc.about.tab3.text2.3"));
+    sb.append(" :\n");
+    sb.append(App.resources.getString("cc.about.tab3.text2.4"));
+    sb.append(" :\n");
+    sb.append(App.resources.getString("cc.about.tab3.text2.5"));
+    sb.append(". . . . . . . . . . . . . . . . . . . .\n");
+    sb.append("\nThis function is not yet available - Feel free to write the code needed ;-) ");
    	return sb.toString();
   }
   
