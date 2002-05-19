e8db05df4113bf200625b6b61324dbd17028a14c
island1 <island1@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2002-05-19 12:46:02 +0000

    a border has been added
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@154 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/src/core/genj/app/AuthorsPanel.java
+++ genj/src/core/genj/app/AuthorsPanel.java
@@ -20,7 +20,7 @@
  * About Menu class - Authors
  * This class creates the content on the Authors tabbed pane in the
  * About Menu application
- * $Header: /cygdrive/c/temp/cvs/genj/genj/src/core/genj/app/AuthorsPanel.java,v 1.3 2002-05-16 21:03:03 island1 Exp $
+ * $Header: /cygdrive/c/temp/cvs/genj/genj/src/core/genj/app/AuthorsPanel.java,v 1.4 2002-05-19 12:46:02 island1 Exp $
  * @author Francois Massonneau <frmas@free.fr>
  * @version 1.0
  *
@@ -30,6 +30,7 @@ package genj.app;
 
 // Import for buttons, labels, and images
 import javax.swing.*;
+import javax.swing.border.*;
 // Import for layout manager.
 import java.awt.*;
 
@@ -41,9 +42,11 @@ public class AuthorsPanel extends JPanel
   {  // Opens constructor
     // Sets the layout by instantiating a BorderLayout container in the setLayout method
     setLayout(new BorderLayout());
-    // Sets the background color for this copyright panel object
+    // Sets the background color for this Authors panel object
     setBackground(Color.white);
     
+    // We create a green border
+    setBorder(BorderFactory.createMatteBorder(1, 1, 3, 3, Color.green));
     
   }  // Closes constructor
   
