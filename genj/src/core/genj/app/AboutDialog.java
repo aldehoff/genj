33b77452cae7e86b45848548a444bd5ecfd572aa
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2002-08-08 20:18:16 +0000

    Refactored AboutDialog, preparation for Look&Feel Panel
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@284 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/src/core/genj/app/AboutDialog.java
+++ genj/src/core/genj/app/AboutDialog.java
@@ -20,7 +20,7 @@
  *
  * AboutDialog class
  * This class creates the content of AboutDialog application
- * $Header: /cygdrive/c/temp/cvs/genj/genj/src/core/genj/app/AboutDialog.java,v 1.2 2002-05-21 21:12:07 island1 Exp $
+ * $Header: /cygdrive/c/temp/cvs/genj/genj/src/core/genj/app/AboutDialog.java,v 1.3 2002-08-08 20:18:13 nmeier Exp $
  * @author Francois Massonneau <frmas@free.fr>
  * @version 1.0
  *
@@ -28,122 +28,225 @@
  
 package genj.app;
 
-import genj.util.*;
-import genj.util.swing.*;
+import genj.Version;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
-import java.awt.*;
-import java.awt.event.*;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 
-import javax.swing.*;
-import javax.swing.event.*;
-
-
-// AboutDialog class is opened
+/**
+ * AboutDialog 
+ */
 public class AboutDialog {
   
-  
-  // Data field
-  public static AboutDialog instance;
-  private JFrame adFrame;
-  private JPanel adMainpanel;
-  private JPanel adNorthpanel;
-  private JPanel adSouthpanel;
-  private JTabbedPane adCenterpanel;
-  private JLabel jlabadNorthpanel;
-  
-//  private JTabbedPane aboutPane;
-  
-  public AboutDialog()  
-  { // Opens AboutDialog constructor
+  /** one static frame we keep around */
+  private static JFrame frame;
+
+  /**
+   * main
+   */
+  public static void showAboutDialog() {
     
-    // We create the About Dialog frame
-    adFrame = new JFrame(App.resources.getString("cc.about.dialog.frame"));
-    // Now the About Dialog (main) panel
-    adMainpanel = new JPanel();
-    // Set layout for About Dialog panel
-    adMainpanel.setLayout(new BorderLayout());
-    // Set background color for About Dialog panel
-    adMainpanel.setBackground(Color.yellow);
-    
-    buildNorthpanel();
-    
-    buildCenterpanel();
-    
-    buildSouthpanel();
-    
-    // Now we add the 3 panels above to the Main panel's border layout manager
-    adMainpanel.add(adNorthpanel, BorderLayout.NORTH);
-    adMainpanel.add(adCenterpanel, BorderLayout.CENTER);
-    adMainpanel.add(adSouthpanel, BorderLayout.SOUTH);
-    
-    adFrame.getContentPane().add(adMainpanel);
-    adFrame.pack();
-    adFrame.setSize(570, 360);
-    adFrame.setBackground(Color.white);
-    adFrame.setVisible(true);
-    
-  } // Closes AboutDialog constructor
-  
-  
-  // Methods
-  
-  // Method to create a panel called adNorthpanel
-  private void buildNorthpanel()
-  { // Opens method buildNorthpanel
-    adNorthpanel = new JPanel();
-    adNorthpanel.setLayout(new BorderLayout());
-    adNorthpanel.setBackground(Color.white);
-    jlabadNorthpanel = new JLabel(App.resources.getString("cc.about.dialog.northpanel.label"), null, JLabel.CENTER);
-    jlabadNorthpanel.setFont(new Font("Times-Roman", Font.BOLD, 12));
-    adNorthpanel.add(jlabadNorthpanel, BorderLayout.CENTER);
-  } // Closes method buildNorthpanel
-  
-  // Method to create a panel called adCenterpanel
-  private void buildCenterpanel()
-  { // Opens method buildCenterpanel
-    // Tabbed pane with panels
-    adCenterpanel = new JTabbedPane(SwingConstants.LEFT);
-    adCenterpanel.setBackground(Color.blue);
-    adCenterpanel.setForeground(Color.white);
-    populateTabbedPane();
-  } // Closes method buildCenterpanel
-  
-  // Method to create a panel called adSouthpanel
-  private void buildSouthpanel()
-  { // Opens method buildSouthpanel
-    adSouthpanel = new JPanel();
-    adSouthpanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
-    adSouthpanel.setBackground(Color.white);
-    JButton jbExitButton = new JButton(App.resources.getString("cc.about.dialog.exit"));
-    jbExitButton.addActionListener(new ActionListener()
-    {
-      public void actionPerformed(ActionEvent e) {
-        instance = null;
-        adFrame.dispose();
-      }
-    });
-    adSouthpanel.add(jbExitButton);
-  } // Closes method buildSouthpanel
-  
-  // method to populate tabbed pane
-  private void populateTabbedPane()
-  {
-    adCenterpanel.addTab(App.resources.getString("cc.about.dialog.tab1.title"), null, new WelcomePanel(), App.resources.getString("cc.about.dialog.tab1.title.tip"));
-    adCenterpanel.addTab(App.resources.getString("cc.about.dialog.tab2.title"), null, new AuthorsPanel(), App.resources.getString("cc.about.dialog.tab2.title.tip"));
-    adCenterpanel.addTab(App.resources.getString("cc.about.dialog.tab3.title"), null, new FeedbackPanel(), App.resources.getString("cc.about.dialog.tab3.title.tip"));
-    adCenterpanel.addTab(App.resources.getString("cc.about.dialog.tab4.title"), null, new CopyrightPanel(), App.resources.getString("cc.about.dialog.tab4.title.tip"));
-  }
-  
-  // main method
-  public static void showAboutDialog()
-  {
-    if (instance!=null) {
-      instance.adFrame.show();
+    // got a frame already?
+    if (frame!=null) {
+      frame.show();
       return;
     }
-    else {
-      instance = new AboutDialog();
+    
+    // create a north panel
+    JLabel pNorth = new JLabel(App.resources.getString("cc.about.dialog.northpanel.label"), null, JLabel.CENTER);
+    
+    // create a south panel
+    JPanel pSouth = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+    JButton bExit = new JButton(App.resources.getString("cc.about.dialog.exit"));
+    bExit.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        frame.hide();
+      }
+    });
+    pSouth.add(bExit);
+
+    // create a center panel
+    JTabbedPane pCenter = new JTabbedPane(SwingConstants.LEFT);
+    pCenter.addTab(App.resources.getString("cc.about.dialog.tab1.title"), null, new WelcomePanel(), App.resources.getString("cc.about.dialog.tab1.title.tip"));
+    pCenter.addTab(App.resources.getString("cc.about.dialog.tab2.title"), null, new AuthorsPanel(), App.resources.getString("cc.about.dialog.tab2.title.tip"));
+    pCenter.addTab(App.resources.getString("cc.about.dialog.tab4.title"), null, new CopyrightPanel(), App.resources.getString("cc.about.dialog.tab4.title.tip"));
+    pCenter.addTab(App.resources.getString("cc.about.dialog.tab5.title"), null, new LookNFeelPanel(), App.resources.getString("cc.about.dialog.tab5.title.tip"));
+
+    // create the main panel    
+    JPanel pMain = new JPanel(new BorderLayout());
+    pMain.add(pNorth , BorderLayout.NORTH );
+    pMain.add(pCenter, BorderLayout.CENTER);
+    pMain.add(pSouth , BorderLayout.SOUTH );
+    
+    // create and show it
+    frame = new JFrame(App.resources.getString("cc.about.dialog.frame"));
+    frame.getContentPane().add(pMain);
+    frame.setSize(480, 300);
+    frame.show();
+
+    // done    
+  }
+
+  /**
+   * Helper to read text from a file
+   */
+  protected static void readTextFile(JTextArea ta, String file, String fallback) {
+    try {
+      FileInputStream fin = new FileInputStream(file);
+      Reader in = new InputStreamReader(fin);
+      ta.read(in,null);
+      fin.close();
     }
+    catch (Exception e) {
+      ta.setText(fallback);
+    }
+  }
+
+  /**
+   * Panel - Authors
+   */  
+  private static class AuthorsPanel extends JScrollPane {
+
+    /** 
+     * Constructor
+     */  
+    protected AuthorsPanel() {
+
+      // create contained text area
+      JTextArea text = new JTextArea();
+      text.setLineWrap(true);
+      text.setWrapStyleWord(true);
+      text.setEditable(false);
+      
+      String path = System.getProperty("user.dir") + File.separatorChar + "doc" + File.separatorChar + "authors.txt";
+      
+      readTextFile(text, path, App.resources.getString("cc.about.file_missing.text") + path);
+
+      // setup looks
+      setViewportView(text);      
+      
+      setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+      setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+
+      // done
+      
+    }      
+  } // AuthorsPanel
+
+  /**
+   * Panel - Welcome
+   */  
+  private static class WelcomePanel extends JPanel  {
+
+    /**
+     * Constructor
+     */
+    protected WelcomePanel() {
+      
+      super(new BorderLayout());
+        
+      // the text
+      JTextArea text = new JTextArea("\n" + App.resources.getString("cc.about.tab1.text1") +"\n\n");
+      text.setBorder(new EmptyBorder(3, 3, 3, 3));    
+      text.setLineWrap(true);
+      text.setWrapStyleWord(true);
+      text.setEditable(false);
+    
+      // the version
+      JLabel version = new JLabel(App.resources.getString("app.title")+" "+Version.getInstance().toString());
+    
+      // looks
+      add(text, BorderLayout.CENTER);
+      add(version, BorderLayout.SOUTH);
+      
+    }
+    
+  } // WelcomePanel
+  
+  /**
+   * Panel - Copyright
+   */  
+  private static class CopyrightPanel extends JPanel {
+
+    /** 
+     * Constructor
+     */  
+    protected CopyrightPanel()  {
+      
+      super(new BorderLayout());
+      
+      add(getNorth(), BorderLayout.NORTH);
+      add(getCenter(), BorderLayout.CENTER);
+    
+    }
+    
+    /**
+     * Create the north 
+     */
+    private JComponent getNorth() {
+      
+      JTextArea text = new JTextArea(App.resources.getString("cc.about.tab4.text1"));
+      text.setLineWrap(true);
+      text.setWrapStyleWord(true);
+      text.setEditable(false);
+      
+      JPanel panel = new JPanel(new BorderLayout());
+      panel.setBorder(BorderFactory.createCompoundBorder(
+        BorderFactory.createTitledBorder(App.resources.getString("cc.about.tab4.text1.title")),
+        new EmptyBorder(3, 3, 3, 3)
+      ));
+      panel.add(text, BorderLayout.CENTER);
+      
+      return panel;
+    }
+
+    /**
+     * Create the center
+     */
+    private JComponent getCenter() {
+          
+      // the text    
+      JTextArea text = new JTextArea();
+      String path = System.getProperty("user.dir") + File.separatorChar + "doc" + File.separatorChar + "gpl.txt";
+      readTextFile(text, path, App.resources.getString("cc.about.file_missing.text") + path);
+      text.setLineWrap(true);
+      text.setWrapStyleWord(true);
+      text.setEditable(false);
+      text.setBorder(new EmptyBorder(3, 3, 3, 3));
+      
+      // a scroller
+      JScrollPane scroll = new JScrollPane(text);
+      scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+      scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+      scroll.setBorder(BorderFactory.createTitledBorder(App.resources.getString("cc.about.tab4.text2.title")));
+      
+      // done
+      return scroll;
+    }
+  
+  } // CopyrightPanel
+  
+  /**
+   * Panel - Look&Feel
+   */
+  private static class LookNFeelPanel extends JPanel {
   }
   
 } // Closes AboutDialog class
