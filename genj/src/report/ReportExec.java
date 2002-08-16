98888872534f307116073d0591305f5a7eec0a09
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2002-08-16 22:38:39 +0000

    new ReportExec
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@322 fbd980a2-0dff-11df-b4b7-0b05b74d0985

---
+++ genj/src/report/ReportExec.java
@@ -0,0 +1,86 @@
+/**
+ * Reports are Freeware Code Snippets
+ *
+ * This report is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+import genj.gedcom.*;
+import genj.report.*;
+import java.io.*;
+
+/**
+ * GenJ - Report to run an external program
+ * @author Nils Meier nils@meiers.net
+ * @version 0.1
+ */
+public class ReportExec implements Report {
+
+  /**
+   * @see genj.report.Report#getAuthor()
+   */
+  public String getAuthor() {
+    return "Nils Meier <nils@meiers.net>";
+  }
+
+  /**
+   * @see genj.report.Report#getInfo()
+   */
+  public String getInfo() {
+    return "This report prompts for an executable and runs it";
+  }
+
+  /**
+   * @see genj.report.Report#getName()
+   */
+  public String getName() {
+    return "Run Executable";
+  }
+
+  /**
+   * @see genj.report.Report#getVersion()
+   */
+  public String getVersion() {
+    return "0.1";
+  }
+
+  /**
+   * @see genj.report.Report#isReadOnly()
+   */
+  public boolean isReadOnly() {
+    return true;
+  }
+
+  /**
+   * @see genj.report.Report#start(ReportBridge, Gedcom)
+   */
+  public boolean start(ReportBridge bridge, Gedcom gedcom) throws InterruptedException {
+    
+    // get the name of the executable
+    String cmd = bridge.getValueFromUser("Please enter path and name of the executable to run", new String[0], "executables");
+    
+    // run it
+    try {
+      Process process = Runtime.getRuntime().exec(cmd);
+      BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+      while (true) {
+        String line = in.readLine();
+        if (line==null) break;
+        bridge.println(line);
+      }
+    } catch (IOException ioe) {
+      bridge.println("*** Sorry, failed with "+ioe.getMessage());
+    }
+    
+    // done
+    return true;
+  }
+
+  /**
+   * @see genj.report.Report#usesStandardOut()
+   */
+  public boolean usesStandardOut() {
+    return true;
+  }
+
+}
