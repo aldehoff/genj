78894924127c4a3ee6b0d681e3c535323bbfaf2a
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2002-08-20 17:14:44 +0000

    wrong import made it into the source - removed
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@330 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/src/core/genj/edit/EditView.java
+++ genj/src/core/genj/edit/EditView.java
@@ -64,7 +64,6 @@ import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.TreeCellRenderer;
 import javax.swing.tree.TreePath;
-import sun.security.krb5.internal.crypto.p;
 
 /**
  * Component for editing genealogic entity properties
