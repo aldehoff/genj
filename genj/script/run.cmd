cb99e48c8a281b54ceb846208c185d2aded26780
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2002-08-23 19:21:43 +0000

    added support for -Dgenj.debug.file
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@367 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/script/run.cmd
+++ genj/script/run.cmd
@@ -1,21 +1,23 @@
 @echo off
 
+
 echo /**
 echo  * GenJ - Start Script
 echo  * 
-echo  * If something goes wrong now you probably have an
-echo  * incomplete CLASSPATH. Either copy swing.jar to
-echo  * the ./lib directory or specify its location via
-echo  *   set CLASSPATH=c:\swing\swing.jar
-echo  * before starting this script.
+echo  * If something goes wrong now you probably have an incomplete 
+echo  * CLASSPATH. GenJ requires genj.jar which is assumed to reside 
+echo  * in ./lib at this point. If you're JDK 1.1.x you'll also have
+echo  * to provide swing.jar which is expected in ./lib, too.
 echo  *
+echo  * If you encounter runtime problems open genj.log and send it
+echo  * as an attachement to genj-developer@lists.sourceforge.net.
+echo  * Also include a description of the problem.
+echo  * 
 echo  */
-
-rem switch off JIT by uncommenting next line
-rem set JAVA_COMPILER=none
 
 set LANG=.\lib\genj_de.zip;.\lib\genj_en.zip;.\lib\genj_fr.zip;.\lib\genj_it.zip
 set CLASSPATH=.\lib\genj.jar;.\lib\swing.jar;.\lib\jhbasic.jar;%LANG%;%CLASSPATH%
 
-java genj.app.App
+java -Dgenj.debug.file=.\genj.log genj.app.App
+
 
