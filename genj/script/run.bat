821d2dd2125efab0df7aa9f1a7caace325db3e8a
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2002-11-12 18:02:19 +0000

    got rid of swing.jar references
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@717 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/script/run.bat
+++ genj/script/run.bat
@@ -6,8 +6,7 @@ echo  * GenJ - Start Script
 echo  * 
 echo  * If something goes wrong now you probably have an incomplete 
 echo  * CLASSPATH. GenJ requires genj.jar which is assumed to reside 
-echo  * in ./lib at this point. If you're JDK 1.1.x you'll also have
-echo  * to provide swing.jar which is expected in ./lib, too.
+echo  * in ./lib at this point. 
 echo  *
 echo  * If you encounter runtime problems open genj.log and send it
 echo  * as an attachement to genj-developer@lists.sourceforge.net.
@@ -16,7 +15,7 @@ echo  *
 echo  */
 
 set LANG=./lib/genj_de.zip;./lib/genj_en.zip;./lib/genj_fr.zip;./lib/genj_it.zip;./lib/genj_es.zip
-set CLASSPATH=./lib/genj.jar;./lib/swing.jar;./lib/jhbasic.jar;./lib/graphj.jar;%LANG%;%CLASSPATH%
+set CLASSPATH=./lib/genj.jar;./lib/jhbasic.jar;./lib/graphj.jar;%LANG%;%CLASSPATH%
 
 java -Dgenj.debug.file=./genj.log genj.app.App
 
