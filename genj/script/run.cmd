8c794cddd1496d7682d43b9a8553a0923f9ea70d
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2002-11-12 18:29:34 +0000

    switched to java -jar
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@718 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/script/run.cmd
+++ genj/script/run.cmd
@@ -4,9 +4,9 @@
 echo /**
 echo  * GenJ - Start Script
 echo  * 
-echo  * If something goes wrong now you probably have an incomplete 
-echo  * CLASSPATH. GenJ requires genj.jar which is assumed to reside 
-echo  * in ./lib at this point. 
+echo  * GenealogyJ requires you to have a Java environment of version
+echo  * 1.3 or higher. Please run 'java -version' in case you're unsure
+echo  * of what's installed on your machine.
 echo  *
 echo  * If you encounter runtime problems open genj.log and send it
 echo  * as an attachement to genj-developer@lists.sourceforge.net.
@@ -14,9 +14,6 @@ echo  * Also include a description of the problem.
 echo  * 
 echo  */
 
-set LANG=.\lib\genj_de.zip;.\lib\genj_en.zip;.\lib\genj_fr.zip;.\lib\genj_it.zip;.\lib\genj_es.zip
-set CLASSPATH=.\lib\genj.jar;.\lib\jhbasic.jar;.\lib\graphj.jar;%LANG%;%CLASSPATH%
-
-java -Dgenj.debug.file=.\genj.log genj.app.App
+java -Dgenj.debug.file=.\genj.log -jar .\lib\genj.jar
 
 
