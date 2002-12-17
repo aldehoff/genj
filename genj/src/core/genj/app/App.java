b90d893a6fb3929ef55d682c80f87bacb522d5f6
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2002-12-17 19:12:45 +0000

    wouldn't create log-file if file is explicitly stated with -Dgenj.debug.file
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@858 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/src/core/genj/app/App.java
+++ genj/src/core/genj/app/App.java
@@ -42,6 +42,7 @@ import java.awt.FlowLayout;
 import java.awt.Rectangle;
 import java.awt.geom.Point2D;
 import java.io.File;
+import java.io.IOException;
 import java.util.Date;
 import java.util.Enumeration;
 import java.util.Hashtable;
@@ -79,13 +80,13 @@ public class App {
     // Startup Information
     Debug.log(Debug.INFO, App.class, "GenJ App - Version "+Version.getInstance()+" - "+new Date());
     String log = EnvironmentChecker.getProperty(this, new String[]{"genj.debug.file", "user.home"}, null, "choose log-file");
-    if (log!=null) {
+    if (log!=null) try {
       File file = new File(log);
-      if (file.exists()) {
-        if (file.isDirectory()) file = new File(file, "genj.log");
+      if (file.isDirectory()) file = new File(file, "genj.log");
+      if (file.createNewFile()) {
         Debug.setFile(file);
       }
-    }
+    } catch (IOException e) {}
     EnvironmentChecker.log();
     
     // init our data
