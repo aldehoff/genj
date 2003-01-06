36265ea64d1a2bf8404dff1c8b801362f5e3f7e0
dkionka <dkionka@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2003-01-06 02:35:18 +0000

    prints out the age of an individual at all events
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@876 fbd980a2-0dff-11df-b4b7-0b05b74d0985

---
+++ genj/src/report/ReportAges.java
@@ -0,0 +1,130 @@
+/**
+ * Reports are Freeware Code Snippets
+ *
+ * This report is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+import genj.gedcom.Fam;
+import genj.gedcom.Gedcom;
+import genj.gedcom.Indi;
+import genj.gedcom.Property;
+import genj.gedcom.PropertyBirth;
+import genj.gedcom.PropertyDate;
+import genj.gedcom.PropertyEvent;
+import genj.report.Report;
+import genj.report.ReportBridge;
+
+/**
+ * GenJ - ReportAges
+ * (based on ReportDescendants)
+ */
+public class ReportAges implements Report {
+
+  /** this report's version */
+  public static final String VERSION = "0.1";
+
+  /**
+   * Returns the version of this script
+   */
+  public String getVersion() {
+    return VERSION;
+  }
+
+  /**
+   * Returns the name of this report - should be localized.
+   */
+  public String getName() {
+    return "Ages";
+  }
+
+  /**
+   * Some information about this report
+   * @return Information as String
+   */
+  public String getInfo() {
+    return "This report prints out the age of an individual at all events";
+  }
+
+  /**
+   * Indication of how this reports shows information
+   * to the user. Standard Out here only.
+   */
+  public boolean usesStandardOut() {
+    return true;
+  }
+
+  /**
+   * Author
+   */
+  public String getAuthor() {
+    return "Daniel P. Kionka";
+  }
+
+  /**
+   * Tells whether this report doesn't change information in the Gedcom-file
+   */
+  public boolean isReadOnly() {
+    return true;
+  }
+
+  /**
+   * This method actually starts this report
+   */
+  public boolean start(ReportBridge bridge, Gedcom gedcom) {
+
+    // Show the users in a combo to the user
+    Indi indi = (Indi)bridge.getValueFromUser(
+      "Please select an individual",
+      gedcom.getEntities(Gedcom.INDIVIDUALS).toArray(),
+      null
+    );
+
+    if (indi==null) {
+      return false;
+    }
+
+    // Display the ages
+    iterate(bridge, indi, 1);
+
+    // Done
+    return true;
+  }
+
+  /**
+   * Iterates over events
+   */
+  private void iterate(ReportBridge bridge, Indi indi, int level) {
+
+    // Here comes the individual
+    bridge.println(indi.getName());
+
+    // Give up if no birth date
+    if (indi.getBirthDate() == null) {
+      bridge.println("no birth date");
+      return;
+    }
+
+    // And we loop through its properties
+    int fcount = indi.getNoOfProperties();
+    for (int f=0;f<fcount;f++) {
+      Property prop = indi.getProperty(f);
+      if ( prop.isValid() && (prop instanceof PropertyEvent) ) {
+
+        // we found an event
+        PropertyEvent event = (PropertyEvent) prop;
+        PropertyDate pDate = event.getDate();
+        if (pDate != null) {
+          String out = event.getTag() + ": " + pDate;
+          if (! (event instanceof PropertyBirth))
+            out += ", " + indi.getAge(pDate);
+          bridge.println(out);
+        }
+      }
+    }
+
+    // in case no DEAT, or if he were alive today
+    bridge.println("Since birth: " + indi.getAge(null));
+  }
+
+} //ReportAges
