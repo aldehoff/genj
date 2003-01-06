8cf7ca7efc815d15b70cec143824a91dd86a79b2
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2003-01-06 20:27:17 +0000

    got rid of unnecessary import
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@877 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/src/report/ReportAges.java
+++ genj/src/report/ReportAges.java
@@ -5,7 +5,6 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
-import genj.gedcom.Fam;
 import genj.gedcom.Gedcom;
 import genj.gedcom.Indi;
 import genj.gedcom.Property;
