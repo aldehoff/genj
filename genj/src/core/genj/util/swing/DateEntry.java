58abca73d801cf597db48c67053e66c9a9f45aa6
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2003-03-24 23:39:21 +0000

    Tomas Dahlqvist fix for US, European and ISO handling of Date
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@1037 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/src/core/genj/util/swing/DateEntry.java
+++ genj/src/core/genj/util/swing/DateEntry.java
@@ -23,6 +23,7 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.text.SimpleDateFormat;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -32,14 +33,18 @@ import javax.swing.event.DocumentListener;
 
 /**
  * Generic component for editing dates
+ * @author Tomas Dahlqvist fix for US, European and ISO handling of Date
  */
-public class DateEntry extends JPanel implements DocumentListener, FocusListener {
+public class DateEntry extends JPanel {
 
   /** field for displaying (static) validity status */
   private JLabel lStatus;
 
   /** text-fields for day,month,year */
   private JTextField tfDay,tfMonth,tfYear;
+  
+  /** fields in order */
+  private JTextField[] tfs = new JTextField[3];
 
   /** state change flag */
   private boolean changed;
@@ -48,7 +53,7 @@ public class DateEntry extends JPanel implements DocumentListener, FocusListener
   private ImageIcon
     imgGood = new ImageIcon(this,"DateGood.gif"),
     imgBad  = new ImageIcon(this,"DateBad.gif" );
-
+    
   /**
    * Constructor
    */
@@ -58,33 +63,34 @@ public class DateEntry extends JPanel implements DocumentListener, FocusListener
     tfDay   = new JTextField( (setDay  != null) ? setDay  .toString() : "",2+1);
     tfMonth = new JTextField( (setMonth!= null) ? setMonth.toString() : "",2+1);
     tfYear  = new JTextField( (setYear != null) ? setYear .toString() : "",4+1);
+    // order fields
+    String format;
+    switch (new SimpleDateFormat().toPattern().charAt(0)) {
+      case 'm': case 'M':
+        tfs[0] = tfMonth; tfs[1] = tfDay  ; tfs[2] = tfYear; format = "mm/dd/yyyy"; break;
+      case 'd': case 'D':
+        tfs[0] = tfDay  ; tfs[1] = tfMonth; tfs[2] = tfYear; format = "dd.mm.yyyy"; break;
+      default: 
+        tfs[0] = tfYear ; tfs[1] = tfMonth; tfs[2] = tfDay ; format = "yyyy-mm-dd"; break;
+    }
     // Listeners
-    tfDay  .getDocument().addDocumentListener(this);
-    tfMonth.getDocument().addDocumentListener(this);
-    tfYear .getDocument().addDocumentListener(this);
-    tfDay  .addFocusListener(this);
-    tfMonth.addFocusListener(this);
-    tfYear .addFocusListener(this);
-    addFocusListener(this);
+    Events e = new Events();
+    tfDay  .getDocument().addDocumentListener(e);
+    tfMonth.getDocument().addDocumentListener(e);
+    tfYear .getDocument().addDocumentListener(e);
+    tfDay  .addFocusListener(e);
+    tfMonth.addFocusListener(e);
+    tfYear .addFocusListener(e);
+    addFocusListener(e);
     // Layout
     setLayout(new FlowLayout(FlowLayout.LEFT));
     add(lStatus);
-    add(tfDay);
-    add(tfMonth);
-    add(tfYear);
-    add(new JLabel("dd/mm/yyyy"));	// dkionka: I assumed mm/dd/yyyy
+    for (int i=0;i<tfs.length;i++) add(tfs[i]);
+    add(new JLabel(format));
     // Status
     changed=false;
     checkStatus();
     // Done
-  }
-
-  /**
-   * Day/Month/Year has changed
-   */
-  public void changedUpdate(DocumentEvent e) {
-    changed=true;
-    checkStatus();
   }
 
   /**
@@ -116,26 +122,6 @@ public class DateEntry extends JPanel implements DocumentListener, FocusListener
     }
 
     // Done
-  }
-
-  /**
-   * Day/Month/Year got focus
-   */
-  public void focusGained(FocusEvent e) {
-    // me?
-    if (e.getSource()==this) {
-      tfDay.requestFocus();
-      return;
-    }
-    // textfield!
-    JTextField tf = (JTextField) e.getSource();
-    tf.selectAll();
-  }
-
-  /**
-   * Day/Month/Year lost focus
-  */
-  public void focusLost(FocusEvent e) {
   }
 
   /**
@@ -186,33 +172,6 @@ public class DateEntry extends JPanel implements DocumentListener, FocusListener
   }
 
   /**
-   * Day/Month/Year has changed
-   */
-  public void insertUpdate(DocumentEvent e) {
-    changed=true;
-    checkStatus();
-
-    if (e.getDocument() == tfDay.getDocument()) {
-      if (tfDay.getText().length()==2)
-      tfMonth.requestFocus();
-      return;
-    }
-    if (e.getDocument() == tfMonth.getDocument()) {
-      if (tfMonth.getText().length()==2)
-      tfYear.requestFocus();
-      return;
-    }
-  }
-
-  /**
-   * Day/Month/Year has changed
-   */
-  public void removeUpdate(DocumentEvent e) {
-    changed=true;
-    checkStatus();
-  }
-
-  /**
    * Sets the date
    */
   public void setDate(Integer setYear, Integer setMonth, Integer setDay) {
@@ -226,8 +185,54 @@ public class DateEntry extends JPanel implements DocumentListener, FocusListener
    * Dis-/Enables the control
    */
   public void setEnabled(boolean set) {
-    tfDay  .setEnabled(set);
-    tfMonth.setEnabled(set);
-    tfYear .setEnabled(set);
+    for (int i=0;i<tfs.length;i++) tfs[i].setEnabled(set);
   }
-}
+  
+  /**
+   * Glue to events
+   */
+  private class Events implements DocumentListener, FocusListener {
+    /** note change */
+    private void changed() {
+      changed=true;
+      checkStatus();
+    }
+    /** callback - text updated */
+    public void changedUpdate(DocumentEvent e) {
+      changed();
+    }
+    /** callback - text inserted */
+    public void insertUpdate(DocumentEvent e) {
+      changed();
+      // jump to next?
+      Object doc = e.getDocument();
+      for (int i=0;i<tfs.length-1;i++) {
+        JTextField tf = tfs[i]; 
+        if (tf.getDocument()==doc && tf.getText().length()==tf.getColumns()-1) {
+          tfs[i+1].requestFocus();
+          break;
+        }
+      }
+      // done
+    }
+    /** callback - text removed */
+    public void removeUpdate(DocumentEvent e) {
+      changed();
+    }
+    /** callback - focus gained */
+    public void focusGained(FocusEvent e) {
+      // me?
+      if (e.getSource()==DateEntry.this) {
+        tfs[0].requestFocus();
+        return;
+      }
+      // one of the textfields!
+      JTextField tf = (JTextField) e.getSource();
+      tf.selectAll();
+    }
+    /** callback - focus lost */
+    public void focusLost(FocusEvent e) {
+    }
+  } //Events
+  
+} //DateEntry
