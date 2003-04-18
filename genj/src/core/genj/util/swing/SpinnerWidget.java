935e14ebd8b9954502d2651dfdc995177a4f98cf
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2003-04-18 04:17:31 +0000

    commit on focus lost
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@1187 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/src/core/genj/util/swing/SpinnerWidget.java
+++ genj/src/core/genj/util/swing/SpinnerWidget.java
@@ -24,6 +24,8 @@ import genj.util.GridBagHelper;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 import java.text.Format;
 import java.util.ArrayList;
 import java.util.List;
@@ -93,6 +95,15 @@ public class SpinnerWidget extends JPanel {
         tfield.setText(format(model.getValue()));
       }
     };
+    
+    tfield.addFocusListener(new FocusAdapter() {
+      /**
+       * @see genj.util.swing.SpinnerWidget#focusLost(java.awt.event.FocusEvent)
+       */
+      public void focusLost(FocusEvent e) {
+        commit();
+      }
+    });
     
     // remember model
     setModel(setModel);
