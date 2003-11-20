bfd26ebbdcb3cb9421454f0c75573ff5b29a8e92
cmuessig <cmuessig@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2003-11-20 21:09:38 +0000

    new: report death and marriages
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@1872 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/src/report/ReportBirthdays.java
+++ genj/src/report/ReportBirthdays.java
@@ -8,12 +8,15 @@
 
 import genj.gedcom.Entity;
 import genj.gedcom.Gedcom;
+import genj.gedcom.Fam;
 import genj.gedcom.Indi;
 import genj.gedcom.PointInTime;
 import genj.gedcom.PropertyDate;
 import genj.report.Report;
+import genj.util.ReferenceSet;
 
-import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * GenJ - Report
@@ -24,15 +27,21 @@ public class ReportBirthdays extends Report {
     
     /** whether we sort by day-of-month or date */
     public boolean isSortDay = true;
+    public boolean reportBirth = true;
+    public boolean reportDeath = true;
+    public boolean reportMarriage = true;
     
     /** persons to report
-     * year > 0 : persons born in year or later
+     * year > 0 : persons with event in this year or later
      * year <= 0: all persons
      */
-    public int startYear = 0;
+    public int year = 0;
     
     /** this report's version */
-    public static final String VERSION = "0.3";
+    public static final String VERSION = "0.4";
+    
+    private static final int DAY = 0;
+    private static final int YEAR = 1;
     
     /**
      * Returns the version of this script
@@ -85,62 +94,106 @@ public class ReportBirthdays extends Report {
         while (month<months.length&&months[month]!=selection)
             month++;
         
-        // Loop through individuals - folks with birthdays in given month
-        Entity[] indis;
-        if (isSortDay) {
+        ArrayList indis = new ArrayList(gedcom.getEntities(gedcom.INDI));
+        ReferenceSet births = new ReferenceSet(), marriages = new ReferenceSet(), deaths = new ReferenceSet();
+        
+        for(int i=0;i<indis.size();i++) {
             
-            // by day of month
-            Comparator comparator = new Comparator() {
-                public int compare(Object o1, Object o2) {
-                    // O.K. here are the birthdays (might be null!)
-                    PropertyDate b1 = ((Indi)o1).getBirthDate();
-                    PropertyDate b2 = ((Indi)o2).getBirthDate();
-                    
-                    // So we check whether we can get the day information
-                    int
-                    d1 = b1!=null ? b1.getStart().getDay() : 0,
-                    d2 = b2!=null ? b2.getStart().getDay() : 0;
-                    
-                    // Comparison at last
-                    return d1-d2;
+            Indi indi = (Indi)indis.get(i);
+            if (isSortDay) {
+                if(reportBirth) {
+                    if((indi.getBirthDate()!=null)&&(indi.getBirthDate().getStart()!=null))
+                        addToReferenceSet(indi.getBirthDate(), indi, births, month, DAY);
                 }
-            }; //Comparator
-            
-            indis = gedcom.getEntities(gedcom.INDI, comparator);
-            
-        } else {
-            
-            // by date
-            indis = gedcom.getEntities(gedcom.INDI, "INDI:BIRT:DATE");
-            
+                if(reportMarriage) {
+                    Fam[] fams = indi.getFamilies();
+                    for(int j=0;j<fams.length;j++) {
+                        if((fams[j].getMarriageDate()!=null)&&(fams[j].getMarriageDate().getStart()!=null))
+                            addToReferenceSet(fams[j].getMarriageDate(), indi, marriages, month, DAY);
+                    }
+                }
+                if(reportDeath) {
+                    if((indi.getDeathDate()!=null)&&(indi.getDeathDate().getStart()!=null))
+                        addToReferenceSet(indi.getDeathDate(), indi, deaths, month, DAY);
+                }
+            }
+            else {
+                if(reportBirth) {
+                    if((indi.getBirthDate()!=null)&&(indi.getBirthDate().getStart()!=null))
+                        addToReferenceSet(indi.getBirthDate(), indi, births, month, YEAR);
+                }
+                if(reportMarriage) {
+                    Fam[] fams = indi.getFamilies();
+                    for(int j=0;j<fams.length;j++) {
+                        if((fams[j].getMarriageDate()!=null)&&(fams[j].getMarriageDate().getStart()!=null))
+                            addToReferenceSet(fams[j].getMarriageDate(), indi, marriages, month, YEAR);
+                    }
+                }
+                if(reportDeath) {
+                    if((indi.getDeathDate()!=null)&&(indi.getDeathDate().getStart()!=null))
+                        addToReferenceSet(indi.getDeathDate(), indi, deaths, month, YEAR);
+                }
+            }
         }
         
-        if(startYear>0) {
-            String[] msgargs = {Integer.toString(startYear),selection};
+        if(year>0) {
+            String[] msgargs = {Integer.toString(year),selection};
             println(i18n("resultYear", msgargs));
         }
         else
             println(i18n("resultAll", selection));
         println();
-        
-        for (int i=0;i<indis.length;i++) {
-            
-            Indi indi = (Indi)indis[i];
-            
-            PropertyDate birth = indi.getBirthDate();
-            if (birth==null)
-                continue;
-            
-            if (birth.getStart().getMonth() != month)
-                continue;
-            
-            if ((startYear!=0)&&(birth.getStart().getYear() < startYear))
-                continue;
-            
-            String[] msgargs = {indi.getId(),indi.getName(),indi.getBirthDate()+""};
-            println(i18n("format",msgargs));
+        if(reportBirth) {
+            println("   "+i18n("birth"));
+            report(births);
+            println();
         }
-        
-        // Done
-    }    
-} //ReportBirthdays
+        if(reportMarriage) {
+            println("   "+i18n("marriage"));
+            report(marriages);
+            println();
+        }
+        if(reportDeath) {
+            println("   "+i18n("death"));
+            report(deaths);
+        }
+    }
+    
+    private void addToReferenceSet(PropertyDate date, Indi indi, ReferenceSet set, int month, int sortOrder) {
+        if((date.getStart().getYear()>=year)&&(date.getStart().getMonth()==month)) {
+            if(sortOrder==DAY) {
+                int day = date.getStart().getDay();
+                if(set.getReferences(new Integer(day)).size()==0)
+                    set.add(new Integer(day), new ReferenceSet());
+                ReferenceSet r = (ReferenceSet)set.getReferences(new Integer(day)).iterator().next();
+                r.add(date.getStart(), indi);
+            }
+            if(sortOrder==YEAR)
+                set.add(date.getStart(), indi);
+        }
+    }
+    
+    private void report(ReferenceSet indis) {
+        Iterator it = indis.getKeys(true).iterator();
+        while(it.hasNext()) {
+            Object key = it.next();
+            if(key instanceof PointInTime)
+                output(indis, (PointInTime)key);
+            if(key instanceof Integer) {
+                ReferenceSet[] dates = (ReferenceSet[])indis.getReferences((Integer)key).toArray(new ReferenceSet[indis.getSize((Integer)key)]);
+                for(int i=0;i<dates.length;i++) {
+                    key = dates[i].getKeys().iterator().next();
+                    output(dates[i], (PointInTime)key);
+                }
+            }
+        }
+    }
+    
+    private void output(ReferenceSet indis, PointInTime date) {
+        ArrayList list = new ArrayList(indis.getReferences(date));
+        for(int i=0;i<list.size();i++) {
+            Indi indi = (Indi)list.get(i);
+            println("      "+date+" @"+indi.getId()+"@ "+indi.getName());
+        }
+    }
+}
