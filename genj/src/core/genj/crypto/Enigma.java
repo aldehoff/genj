3ca3250d15d95c3898cd449b1ad1fa1180024a6c
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2003-12-12 20:38:48 +0000

    cryptography support (for private/public de/encoding)
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@1894 fbd980a2-0dff-11df-b4b7-0b05b74d0985

---
+++ genj/src/core/genj/crypto/Enigma.java
@@ -0,0 +1,65 @@
+/**
+ * GenJ - GenealogyJ
+ *
+ * Copyright (C) 1997 - 2002 Nils Meier <nils@meiers.net>
+ *
+ * This piece of code is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package genj.crypto;
+
+import java.io.IOException;
+
+/**
+ * The abstract base type for en/decrypting Gedcom information 
+ */
+public abstract class Enigma {
+  
+  /** the implementation class we use */
+  private static final String DES = "genj.crypto.EnigmaImpl";
+  
+  /**
+   * Get access to an Enigma instance
+   * @return instance of Enigma or null if no de/encryption implementation available 
+   */
+  public static Enigma getInstance(String password) {
+    
+    try {
+      return ((Enigma)Class.forName(DES).newInstance()).init(password);
+    } catch (Throwable t) {
+      return null;
+    }
+    
+  }
+  
+  /**
+   * Initialization call
+   */
+  protected abstract Enigma init(String password);
+  
+  /**
+   * encrypt
+   * @param value the plain data as Java string to encrypt
+   * @return the encrypted value
+   */  
+  public abstract String encrypt(String value) throws IOException;
+
+  /**
+   * decrypt
+   * @param value the encrypted data as Java string to decrypt 
+   * @return the decrypted value
+   */  
+  public abstract String decrypt(String value) throws IOException;
+  
+} //Enigma
