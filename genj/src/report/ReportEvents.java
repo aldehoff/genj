7aea56be67c9075922d6f13467dbdfd6ae709464
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2004-01-14 19:24:03 +0000

    allow to disregard dead when looking for events
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@1908 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/src/report/ReportEvents.java
+++ genj/src/report/ReportEvents.java
@@ -27,17 +27,19 @@ import java.util.Iterator;
  * @author Carsten Müssig carsten.muessig@gmx.net
  */
 public class ReportEvents extends Report {
-    
+
     /** whether we sort by day-of-month or date */
     public boolean isSortDay = true;
     /** whether births should be reported */
     public boolean reportBirth = true;
+    /** wether dead persons should be shown */
+    public boolean showDead = true;
     /** whether deaths should be reported */
     public boolean reportDeath = true;
     /** whether marriages should be reported */
     public boolean reportMarriage = true;
     /** whether divorces should be reported */
-    public boolean reportDivorce = true;    
+    public boolean reportDivorce = true;
     /** day of the date limit */
     public int day = new GregorianCalendar().get(Calendar.DAY_OF_MONTH);
     public String[] days = { "1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31" };
@@ -52,39 +54,39 @@ public class ReportEvents extends Report {
     /** how the day should be handled */
     public int handleMonth = 2;
     public String[] handleMonths = { i18n("date.min"), i18n("date.max"), i18n("date.fix"), i18n("date.ignore")};
-    
+
     /** how the day should be handled */
     public int handleYear = 2;
     public String[] handleYears = { i18n("date.min"), i18n("date.max"), i18n("date.fix"), i18n("date.ignore")};
-    
+
     /** this report's version */
     public static final String VERSION = "1.0";
-    
+
     /**
      * Returns the version of this script
      */
     public String getVersion() {
         return VERSION;
     }
-    
+
     /**
      * Returns the name of this report
      */
     public String getName() {
         return i18n("name");
     }
-    
+
     /**
      * Some information about this report
      */
     public String getInfo() {
         return i18n("info");
     }
-    
+
     public String getAuthor() {
         return "Nils Meier <nils@meiers.net>, Carsten M\u00FCssig <carsten.muessig@gmx.net>";
     }
-    
+
     /**
      * @see genj.report.Report#accepts(java.lang.Object)
      */
@@ -92,7 +94,7 @@ public class ReportEvents extends Report {
         // we accept only GEDCOM
         return context instanceof Gedcom ? getName() : null;
     }
-    
+
     /**
      * Entry point into this report - by default reports are only run on a
      * context of type Gedcom. Depending on the logic in accepts either
@@ -102,17 +104,21 @@ public class ReportEvents extends Report {
         if((reportBirth)||(reportDeath)||(reportMarriage)||(reportDivorce)) {
             // assuming Gedcom
             Gedcom gedcom = (Gedcom)context;
-            
+
             ArrayList indis = new ArrayList(gedcom.getEntities(gedcom.INDI));
             ReferenceSet births = new ReferenceSet(), marriages = new ReferenceSet(), divorces = new ReferenceSet(), deaths = new ReferenceSet();
-            
+
             for(int i=0;i<indis.size();i++) {
-                
+
                 Indi indi = (Indi)indis.get(i);
-                
+
                 if(reportBirth) {
                     if((indi.getBirthDate()!=null)&&(indi.getBirthDate().getStart()!=null)&&(checkDate(indi.getBirthDate().getStart())))
-                        addToReferenceSet(indi.getBirthDate().getStart(), indi, births);
+                      if (showDead)
+                          addToReferenceSet(indi.getBirthDate().getStart(), indi, births);
+                        else
+                          if ((indi.getDeathDate()==null)||(indi.getDeathDate().getStart()==null))
+                            addToReferenceSet(indi.getBirthDate().getStart(), indi, births);
                 }
                 if(reportMarriage) {
                     Fam[] fams = indi.getFamilies();
@@ -133,12 +139,12 @@ public class ReportEvents extends Report {
                         addToReferenceSet(indi.getDeathDate().getStart(), indi, deaths);
                 }
             }
-            
+
             println(i18n("day")+": "+(day+1)+" ("+getHandleDate(handleDay)+")");
             println(i18n("month")+": "+(month+1)+" ("+getHandleDate(handleMonth)+")");
             println(i18n("year")+": "+year+" ("+getHandleDate(handleYear)+")");
             println();
-            
+
             if(reportBirth) {
                 println("   "+i18n("birth"));
                 report(births);
@@ -153,14 +159,14 @@ public class ReportEvents extends Report {
                 println("   "+i18n("divorce"));
                 report(divorces);
                 println();
-            }            
+            }
             if(reportDeath) {
                 println("   "+i18n("death"));
                 report(deaths);
             }
         }
     }
-    
+
     /** get an i18n() string with info about date handling
      * @param int value for string lookup */
     private String getHandleDate(int what) {
@@ -172,7 +178,7 @@ public class ReportEvents extends Report {
             default: return "";
         }
     }
-    
+
     /** adds an entry to a RefernceSet
      * @param date of an event
      * @param entity involved in the event
@@ -189,7 +195,7 @@ public class ReportEvents extends Report {
         else
             set.add(date, entity);
     }
-    
+
     /** prepares the data for output / printing which is done via output() calls
      * @param indis data for output */
     private void report(ReferenceSet indis) {
@@ -217,7 +223,7 @@ public class ReportEvents extends Report {
             }
         }
     }
-    
+
     /** prints the report output
      * @param indis data to print
      * @param date of an event */
@@ -237,12 +243,12 @@ public class ReportEvents extends Report {
             }
         }
     }
-    
+
     /* checks if the date of an event matches the given values of the user (fix, max, min, ...)
      * @return boolean to indicate the result */
     private boolean checkDate(PointInTime date) {
         boolean d = false, m = false, y = false;
-        
+
         if((handleDay==0)&&(day<=date.getDay())) // day = minimum
             d = true;
         else if((handleDay==1)&&(day>=date.getDay())) // day = maximum
@@ -251,7 +257,7 @@ public class ReportEvents extends Report {
             d = true;
         else if(handleDay==3) // day = ignore
             d = true;
-        
+
         if((handleMonth==0)&&(month<=date.getMonth())) // month = minimum
             m = true;
         else if((handleMonth==1)&&(month>=date.getMonth())) // month = maximum
@@ -260,7 +266,7 @@ public class ReportEvents extends Report {
             m = true;
         else if(handleMonth==3) // month = ignore
             m = true;
-        
+
         if((handleYear==0)&&(year<=date.getYear())) // year = minimum
             y = true;
         else if((handleYear==1)&&(year>=date.getYear())) // year = maximum
@@ -269,9 +275,10 @@ public class ReportEvents extends Report {
             y = true;
         else if(handleYear==3) // year = ignore
             y = true;
-        
+
         if((d)&&(m)&&(y))
             return true;
         return false;
     }
-}
+    
+} //ReportEvents
