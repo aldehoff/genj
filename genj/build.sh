704d9a4ca584724407cead49144a70a3a4efbe94
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2004-03-01 05:48:57 +0000

    moved echo statements into build.xml so all platforms can enjoy them added $ANT_HOME/ant.jar to Classpath removed default of doing a clean&dist - user should specify required option
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@2019 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/build.sh
+++ genj/build.sh
@@ -1,41 +1,7 @@
 #!/bin/sh
 #
 
-export CLASSPATH=$CLASSPATH:$ANT_HOME/lib/ant.jar
+export CLASSPATH=$CLASSPATH:$ANT_HOME/lib/ant.jar:$ANT_HOME/ant.jar
 
-if [[ $1 = "" ]] ; then
+$JAVA_HOME/bin/java -Dant.home=$ANT_HOME org.apache.tools.ant.Main $1 $2 $3
 
-	$JAVA_HOME/bin/java -Dant.home=$ANT_HOME org.apache.tools.ant.Main clean
-	$JAVA_HOME/bin/java -Dant.home=$ANT_HOME org.apache.tools.ant.Main dist
-
-elif [[ $1 = "clean" ]] ; then
-	echo "We clean everything"
-	$JAVA_HOME/bin/java -Dant.home=$ANT_HOME org.apache.tools.ant.Main $1
-
-elif [[ $1 = "init" ]] ; then
-	echo "We make the init directories"
-	$JAVA_HOME/bin/java -Dant.home=$ANT_HOME org.apache.tools.ant.Main $1
-	
-elif [[ $1 = "version" ]] ; then
-	echo "We establish the version number"
-	$JAVA_HOME/bin/java -Dant.home=$ANT_HOME org.apache.tools.ant.Main $1
-
-elif [[ $1 = "dist" ]] ; then
-	echo "We make the distribution package"
-	$JAVA_HOME/bin/java -Dant.home=$ANT_HOME org.apache.tools.ant.Main $1
-
-elif [[ $1 = "javadoc" ]] ; then
-	echo "We create the API"
-	$JAVA_HOME/bin/java -Dant.home=$ANT_HOME org.apache.tools.ant.Main $1
-
-elif [[ $1 = "run" ]] ; then
-	echo "We build Genj to test a run"
-	$JAVA_HOME/bin/java -Dant.home=$ANT_HOME org.apache.tools.ant.Main $1
-
-else
-	echo ""
-	echo "Wrong parameter - Please check again a valid parameter."
-	echo ""
-	break
-
-fi
