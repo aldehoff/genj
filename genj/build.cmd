6bf0d06023bd22f6ce175adb6431f985e450ea0b
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2004-05-23 19:36:53 +0000

    fixed fork-option
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@2202 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/build.cmd
+++ genj/build.cmd
@@ -1,9 +1,8 @@
 @echo off
 setlocal
 
-set classpath=%ANT_HOME%/lib/ant.jar;%ANT_HOME%/ant.jar
-set path=%JAVA_HOME%/bin
-java org.apache.tools.ant.Main %1 %2 %3 %4
+set classpath=%JAVA_HOME%/lib/tools.jar;%ANT_HOME%/lib/ant.jar;%ANT_HOME%/ant.jar
+%JAVA_HOME%/bin/java org.apache.tools.ant.Main %1 %2 %3 %4
 
 endlocal
 
