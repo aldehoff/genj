057785ba4a8909b43a4ac64c14de795fba1ba3d5
cmuessig <cmuessig@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2004-06-20 19:49:40 +0000

    added some options
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@2231 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/src/core/genj/report/Options.java
+++ genj/src/core/genj/report/Options.java
@@ -16,8 +16,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- * $Revision: 1.3 $ $Author: nmeier $ $Date: 2004-05-26 20:02:59 $
+ *
+ * $Revision: 1.4 $ $Author: cmuessig $ $Date: 2004-06-20 19:49:40 $
  */
 package genj.report;
 
@@ -31,25 +31,159 @@ import java.util.List;
  * Options for report package
  */
 public class Options implements OptionProvider {
-  
-  /** 'singleton' instance */
-  private static Options instance = new Options();
-  
-  /** option - browser executable */
-  public File browser = new File("");
-
-  /**
-   * callback - provide options
-   */
-  public List getOptions() {
-    return PropertyOption.introspect(getInstance());
-  }
-
-  /**
-   * accessor - singleton instance
-   */
-  public static Options getInstance() {
-    return instance;
-  }
-
+    
+    /** 'singleton' instance */
+    private static Options instance = new Options();
+    
+    /** option - browser executable */
+    private File browser = new File("");
+    
+    /** Positions after decimal point */
+    private int positions = 2;
+    
+    /** indent per level in reports */
+    private int indentPerLevel = 5;
+    
+    /** birth symbol in reports */
+    private String birthSymbol = "*";
+    
+    /** baptism symbol in reports */
+    private String baptismSymbol =  "~";
+    
+    /** engagement symbol in reports */
+    private String engagingSymbol = "o";
+    
+    /** marriage symbol in reports */
+    private String marriageSymbol = "oo";
+    
+    /** divorce symbol in reports */
+    private String divorceSymbol = "o|o";
+    
+    /** death symbol in reports */
+    private String deathSymbol = "+";
+    
+    /** burial symbol in reports */
+    private String burialSymbol = "[]";
+    
+    /** child of symbol in reports */
+    private String childOfSymbol = "/";
+    
+    public int getIndentPerLevel() {
+        return indentPerLevel;
+    }
+    
+    public void setIndentPerLevel(int set) {
+        indentPerLevel = Math.max(2,set);
+    }
+    
+    public int getPositions() {
+        return positions;
+    }
+    
+    public void setPositions(int set) {
+        positions = Math.max(0,set);
+    }
+    
+    public String getBirthSymbol() {
+        return birthSymbol;
+    }
+    
+    public void setBirthSymbol(String set) {
+        if (set!=null&&set.trim().length()>0)
+            birthSymbol = set;
+        else
+            birthSymbol = "*";
+    }
+    
+    public String getBaptismSymbol() {
+        return baptismSymbol;
+    }
+    
+    public void setBaptismSymbol(String set) {
+        if (set!=null&&set.trim().length()>0)
+            baptismSymbol = set;
+        else
+            baptismSymbol = "~";
+    }
+    
+    public String getEngagingSymbol() {
+        return engagingSymbol;
+    }
+    
+    public void setEngagingSymbol(String set) {
+        if (set!=null&&set.trim().length()>0)
+            engagingSymbol = set;
+        else
+            engagingSymbol = "o";
+    }
+    
+    public String getMarriageSymbol() {
+        return marriageSymbol;
+    }
+    
+    public void setMarriageSymbol(String set) {
+        if (set!=null&&set.trim().length()>0)
+            marriageSymbol = set;
+        else
+            marriageSymbol = "oo";
+    }
+    
+    public String getDivorceSymbol() {
+        return divorceSymbol;
+    }
+    
+    public void setDivorceSymbol(String set) {
+        if (set!=null&&set.trim().length()>0)
+            divorceSymbol = set;
+        else
+            divorceSymbol = "o|o";
+    }
+    
+    public String getDeathSymbol() {
+        return deathSymbol;
+    }
+    
+    public void setDeathSymbol(String set) {
+        if (set!=null&&set.trim().length()>0)
+            deathSymbol = set;
+        else
+            deathSymbol = "+";
+    }
+    
+    public String getBurialSymbol() {
+        return burialSymbol;
+    }
+    
+    public void setBurialSymbol(String set) {
+        if (set!=null&&set.trim().length()>0)
+            burialSymbol = set;
+        else
+            burialSymbol = "[]";
+    }
+    
+    public String getChildOfSymbol() {
+        return childOfSymbol;
+    }
+    
+    public void setChildOfSymbol(String set) {
+        if (set!=null&&set.trim().length()>0)
+            childOfSymbol = set;
+        else
+            childOfSymbol = "/";
+    }
+    
+    /**
+     * callback - provide options
+     */
+    public List getOptions() {
+        return PropertyOption.introspect(getInstance());
+    }
+    
+    /**
+     * accessor - singleton instance
+     */
+    public static Options getInstance() {
+        return instance;
+    }
+    
 } //Options
