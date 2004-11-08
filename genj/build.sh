0834b45f5b636f67b3e1995599465de392cff2e9
island1 <island1@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2004-11-08 06:29:56 +0000

    typo fixed
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@2504 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/build.sh
+++ genj/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
 
-export CLASSPATH=$CLASSPATH:$JAVA_HOME/lib/tools.jar:$ANT_HOME/ant.jar:$ANT_HOME/lib/ant.jar:$ANT_HOME/lib/ant-launcher
+export CLASSPATH=$CLASSPATH:$JAVA_HOME/lib/tools.jar:$ANT_HOME/ant.jar:$ANT_HOME/lib/ant.jar:$ANT_HOME/lib/ant-launcher.jar
 $JAVA_HOME/bin/java org.apache.tools.ant.Main $1 $2 $3
 
