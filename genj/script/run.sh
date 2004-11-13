008c09cb351ebbbcf4686f07a4c48dd3e2dd6fa2
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2004-11-13 18:41:57 +0000

    tried to make this more fool-proof + handle case of run.sh being started through symlink + be smart about choosing/finding java
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@2543 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/script/run.sh
+++ genj/script/run.sh
@@ -1,10 +1,38 @@
 #!/bin/sh
 
+# GenJ has to be run from inside it's installation directory
+# so change into directory where this script was started
 cd `dirname $0`
 
-if [ -e "./run.jar" ]; then
-  java -jar run.jar
-else
-  echo "*** ERROR: Missing GenJ resource(s) in "`dirname $0`
+# check the script for being a symbolik link we can follow
+SCRIPT=`basename $0`
+while [ -h "$SCRIPT" ]; do
+ SCRIPT=`ls -l $SCRIPT | grep -o '[/.[:alnum:]]*$'`
+ echo "*** INFO: Following symlink $SCRIPT"
+ cd `dirname $SCRIPT`
+ SCRIPT=`basename $SCRIPT` done
+
+# final check if the GenJ main archive is right here
+if [ ! -e "./run.jar"  ]; then
+ echo "*** ERROR: Missing GenJ resource(s) in "`pwd`
+ exit 1
 fi
 
+echo "*** INFO: Running GenJ from"`pwd`
+
+# find java
+JAVA=$JAVA_HOME/bin/java
+if [ ! -x "$JAVA" ]; then
+ JAVA=`which java`
+ if [ $? -eq 1 ]; then
+  echo "*** ERROR: Can't find java executable"
+  exit 1
+ fi
+fi
+
+# run it
+CMD="$JAVA -jar run.jar"
+
+echo "*** INFO: Executing '$CMD'"
+
+# end
