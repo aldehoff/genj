6cbde9cdc16d41d282da8ef1cd68ac1ca125c682
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2004-12-14 18:29:13 +0000

    new report - show occurances of places along timeline
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@2749 fbd980a2-0dff-11df-b4b7-0b05b74d0985

---
+++ genj/src/report/ReportPlaceHistory.java
@@ -0,0 +1,152 @@
+/**
+ * Reports are Freeware Code Snippets
+ *
+ * This report is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+import genj.chart.Chart;
+import genj.chart.XYSeries;
+import genj.gedcom.Entity;
+import genj.gedcom.Fam;
+import genj.gedcom.Gedcom;
+import genj.gedcom.GedcomException;
+import genj.gedcom.Indi;
+import genj.gedcom.Property;
+import genj.gedcom.PropertyChoiceValue;
+import genj.gedcom.PropertyDate;
+import genj.gedcom.time.PointInTime;
+import genj.report.Report;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * Show a chart of places over time
+ */
+public class ReportPlaceHistory extends Report {
+  
+  private final static String 
+  	PLAC = "PLAC",
+  	DATE = "DATE";
+  
+  /** top n */
+  private int topn = 10;
+  
+  /** resolution */
+  private int resolution = 10;
+
+  /**
+   * Accessor - top n
+   */
+  public int getTopn() {
+    return topn;
+  }
+  
+  /**
+   * Accessor - top n
+   */
+  public void setTopn(int set) {
+    topn = Math.max(0, Math.max(3, set));
+  }
+
+  /**
+   * no need for STDOUT
+   */
+  public boolean usesStandardOut() {
+    return false;
+  }
+  
+  /**
+   * Report's main
+   */
+  public void start(Object context) {
+    
+    // assuming Gedcom
+    Gedcom gedcom = (Gedcom)context;
+    
+    // find series for the top n places
+    Map plac2series = getSeriesForPlaces(gedcom);
+    
+    // loop over individuals
+    Iterator indis = gedcom.getEntities(Gedcom.INDI).iterator();
+    while (indis.hasNext()) {
+      analyze((Indi)indis.next(), plac2series);
+    }
+    
+    // loop over families
+    Iterator fams = gedcom.getEntities(Gedcom.FAM).iterator();
+    while (fams.hasNext()) {
+      analyze((Fam)fams.next(), plac2series);
+    }
+    
+    // get the series array
+    XYSeries[] series = new XYSeries[plac2series.size()];
+    plac2series.values().toArray(series);
+    
+    // show it
+    String title = i18n("title", gedcom.getName());
+    String xaxis = i18n("xaxis", resolution);
+    String yaxis = i18n("yaxis");
+    Chart chart = new Chart(title, xaxis, yaxis, series, null, true);
+    showChartToUser(chart);
+
+    // done
+  }
+  
+  /**
+   * Analyze indi or fam
+   */
+  private void analyze(Entity ent, Map plac2series) {
+    
+    // look into entity's PropertyChoiceValues
+    Iterator it = ent.getProperties(PropertyChoiceValue.class).iterator();
+    while (it.hasNext()) {
+      // see whether it's a PLACe
+      PropertyChoiceValue place = (PropertyChoiceValue)it.next();
+      if (!PLAC.equals(place.getTag()))
+        continue;
+      // check whether we have a series for that PLACe
+      String value = place.getValue();
+      XYSeries series = (XYSeries)plac2series.get(value);
+      if (series==null)
+        continue;
+      // look for a date we could use
+      Property parent = place.getParent();
+      Property date = parent.getProperty(DATE);
+      if (!(date instanceof PropertyDate))
+        continue;
+      // try to add it to series
+      try {
+	      int year = ((PropertyDate)date).getStart().getPointInTime(PointInTime.GREGORIAN).getYear();
+	      if (year!=PointInTime.UNKNOWN)
+	        series.inc( year/resolution*resolution );
+      } catch (GedcomException e) {
+        // ignore dates where we can't get a GREGORIAN time
+      }
+    }
+    
+    // done
+  }
+  
+  /**
+   * Find the top n places and create series for them
+   */
+  private Map getSeriesForPlaces(Gedcom gedcom) {
+    
+    // find what values are in gedcom for PLAC (sorted by ranking)
+    String[] values = PropertyChoiceValue.getChoices(gedcom, PLAC, true);
+    
+    // create series for the top n
+    Map result = new HashMap();
+    for (int s=0; s<values.length&&s<topn; s++) {
+      result.put(values[s], new XYSeries(values[s]));
+    }
+    
+    // done
+    return result;
+  }
+  
+} //ReportPlaceHistory
