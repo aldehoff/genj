2a32b5a31852197ea0807cc39090e9b32120687f
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2005-05-31 20:30:57 +0000

    Setup: Added support for a 'all.home' environment parameter that allows to ask for a shared directory amongst all users (on windows only right now - something like c:\documents and settings\all users)
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@3205 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/src/core/genj/util/EnvironmentChecker.java
+++ genj/src/core/genj/util/EnvironmentChecker.java
@@ -1,9 +1,13 @@
 package genj.util;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
 import java.net.URLClassLoader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EnvironmentChecker {
 
@@ -12,7 +16,7 @@ public class EnvironmentChecker {
         "java.version", "java.class.version",
         "os.name", "os.arch", "os.version",
         "browser", "browser.vendor", "browser.version",
-        "user.name", "user.home", "user.dir"
+        "user.name", "user.home", "user.dir", "all.home"
   };
   
   /**
@@ -113,5 +117,33 @@ public class EnvironmentChecker {
     return fallback;
   }
 
+  /**
+   * Initialize some of our own system properties
+   */
+  static {
+    
+    // prepare query and pattern
+    String QUERY = "reg query \"HKLM\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\ProfileList\"";
+    Pattern PATTERN  = Pattern.compile(".*AllUsersProfile\tREG_SZ\t(.*)");
+    String value = null;
+    
+    // run process and parse output
+    try {
+      Process process = Runtime.getRuntime().exec(QUERY);
+  
+      BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+      while (true) {
+        String line = in.readLine();
+        if (line==null) break;
+        Matcher match = PATTERN.matcher(line);
+        if (match.matches()) {
+          System.setProperty("all.home", new File(new File(System.getProperty("user.home")).getParent(), match.group(1)).toString());
+          break;
+        }
+      }
+    } catch (Throwable t) {
+    }
+    
+  }
   
 }
