f60b2533ab6fde5a236c18bb8b708f01e9e49cee
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2005-07-21 17:40:50 +0000

    report: added support for running the 'common ancestor' on a two-indi-argument context. To try it out select two rows in TableView with ctrl-clicks, then ctrl-right-click, open "2 Individuals" and select this report
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@3443 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/src/report/ReportCommonAncestor.java
+++ genj/src/report/ReportCommonAncestor.java
@@ -24,14 +24,43 @@ public class ReportCommonAncestor extends Report {
   }
   
   /**
-   * our main method
+   * special treatmen context argument check
+   */
+  public String accepts(Object context) {
+    // an indi is fine
+    if (context instanceof Indi)
+      return getName();
+    // an array of indis is fine as well
+    if (context instanceof Indi[]) {
+      Indi[] indis = (Indi[])context;
+      if (indis.length==2)
+        return getName();
+    }
+    // no go
+    return null;
+  }
+  
+  /**
+   * our main method for an argument individual
    */
   public void start(Indi indi) {
-    
+    // ask for other
     Indi other = (Indi)getEntityFromUser(i18n("select"), indi.getGedcom(), Gedcom.INDI);
     if (other==null)
       return;
+    // continue
+    start(new Indi[] { indi, other});
+  }
+  
+  /**
+   * our main method for an argument of a bunch of individuals
+   */
+  public void start(Indi[] indis) {
     
+    // first and second
+    Indi indi = indis[0];
+    Indi other = indis[1];
+      
     // Recurse into indi
     Indi ancestor = getCommonAncestor(indi, other);
 
