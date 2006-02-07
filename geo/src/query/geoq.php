648e8b674c7383e6ce52c2aba7c5ac297f918f1a
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2006-02-07 05:29:49 +0000

    geo: hardened against badly formed query parameters
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@3746 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- geo/src/query/geoq.php
+++ geo/src/query/geoq.php
@@ -2,22 +2,32 @@
 
 	// read stdinput - check header
 	$in = fopen("php://input", "rb");
-	if (trim(fgets($in))!='GEOQ') die('PING');
+	if (trim(fgets($in))!="GEOQ") die("PING");
+	
+	// read password
+	$credentials = file("geoq.ini");
+	if (!$credentials) die("error:credentials");
+	$user = trim($credentials[0]);
+	$pass = trim($credentials[1]);
 	
 	// connect to database
-	$link = mysql_connect('mysql4-g', 'user', 'pass')
-		or die('error:connect ' . mysql_error());
-	mysql_select_db('g46817_geo') 
-		or die('error:db');
+	$link = mysql_connect("mysql4-g", $user, $pass)
+		or die("error:connect " . mysql_error());
+	mysql_select_db("g46817_geo") 
+		or die("error:db");
 
 	// read stdinput - lines "city,jurisdiction,jurisdiction,...,country" by one
-	while ( ($tokens=fgetcsv($in, 100, ",")) !== FALSE) {
+	for ($l=0 ; ($tokens=fgetcsv($in, 100, ",")) !== FALSE ; $l++) {
+	
+		// newline?
+		if ($l>0) echo "\n";
 
 		// at least 3 tokens?
 		if (count($tokens)<3) continue;
 
 		// grab city (first token)
 		$city = trim($tokens[0]);
+		if (strstr($city, "\"")!=FALSE) continue;
 		$like = rtrim($city, "*");
 		if (strlen($like)<3) continue;
 		
@@ -32,32 +42,34 @@
 		$lquery = 
 			"SELECT locations.city, jurisdictions.name, locations.country, locations.lat, locations.lon " .
 			"FROM locations LEFT JOIN jurisdictions ON jurisdictions.jurisdiction=locations.jurisdiction AND jurisdictions.country=locations.country AND jurisdictions.preferred=1 " .
-			"WHERE locations.city".$op."'$city'";
+			"WHERE locations.city".$op."\"$city\"";
 	  
 		// try to lookup country? (last token)
 		$country = trim($tokens[count($tokens)-1]);
+		if (strstr($country, "\"")!=FALSE) continue;
 		if (strlen($country)>0)
-	  		$lquery = "$lquery AND locations.country = '$country'";
+	  		$lquery = "$lquery AND locations.country = \"$country\"";
 	  
 		// check for well known jurisdictions? (tokens 1 to n-1)
 		for ($j=1 ; $j<count($tokens)-1 ; $j++) {
 			// stop at first empty jurisdiction
 			$jurisdiction = trim($tokens[$j]);
+			if (strstr($country, "\"")!=FALSE) break;
 			if ($jurisdiction=="") break;
 
 			// prepare query for matching names
-			$jquery = "SELECT jurisdiction FROM jurisdictions WHERE name LIKE '$jurisdiction'";
+			$jquery = "SELECT jurisdiction FROM jurisdictions WHERE name LIKE \"$jurisdiction\"";
 
 			// add country qualifier if available
 			if (strlen($country)>0)
-		  		$jquery = "$jquery AND country = '$country'";
+		  		$jquery = "$jquery AND country = \"$country\"";
 	
 			// query and apply jurisdiction if found
 			$rows = mysql_query($jquery);
-			if (!$rows) die('error:select');
+			if (!$rows) die("error:selectj");
 	 		if (mysql_num_rows($rows)==1) {
 		  		$row = mysql_fetch_row($rows);
-		  		$lquery = "$lquery AND jurisdictions.jurisdiction = '$row[0]'";
+		  		$lquery = "$lquery AND jurisdictions.jurisdiction = \"$row[0]\"";
 		  		$j = count($tokens);
 	  	}
 			mysql_free_result($rows);
@@ -67,13 +79,12 @@
 
 		// query and return rows
 		$rows = mysql_query($lquery);
-		if (!$rows) die('error:select');
+		if (!$rows) die("error:selectl");
 		for ($i=0 ; $row = mysql_fetch_row($rows) ; $i++) {
 			if ($i>0) echo ";";
 			echo "$row[0],$row[1],$row[2],$row[3],$row[4]";
 		}
 		mysql_free_result($rows);
-		echo "\n";
 
 		// next
 	}
