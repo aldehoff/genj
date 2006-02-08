fb7749571e205d1ed3d80239d00b3e1d35eaa955
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2006-02-08 05:28:44 +0000

    geo: hardened against number conversion problems on result
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@3756 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- geo/src/core/genj/geo/GeoService.java
+++ geo/src/core/genj/geo/GeoService.java
@@ -167,7 +167,12 @@ public class GeoService {
       return null;
     
     GeoLocation result = new GeoLocation(tokens.get(0), tokens.get(1), Country.get(tokens.get(2))); 
-    result.setCoordinate(Float.parseFloat(tokens.get(3)), Float.parseFloat(tokens.get(4)));
+    
+    try {
+      result.setCoordinate(Float.parseFloat(tokens.get(3)), Float.parseFloat(tokens.get(4)));
+    } catch (NumberFormatException e) {
+      return null;
+    }
     
     return result;
   }
