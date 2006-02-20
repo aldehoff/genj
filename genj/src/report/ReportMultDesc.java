ec01c60dc27b1133fa78495f3c760886bbb29de7
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2006-02-20 02:17:32 +0000

    report: migrated Daniel's MultDesc report to the fo.Document pattern - still for me to-do: links and csv from before
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@3893 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/src/report/ReportMultDesc.java
+++ genj/src/report/ReportMultDesc.java
@@ -5,6 +5,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
+import genj.fo.Document;
 import genj.gedcom.Entity;
 import genj.gedcom.Fam;
 import genj.gedcom.Indi;
@@ -13,322 +14,293 @@ import genj.gedcom.Property;
 import genj.gedcom.PropertyMultilineValue;
 import genj.report.Report;
 
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.swing.ImageIcon;
 
 /**
- * GenJ - ReportMultDesc
- * TODO Daniel
- *   titles
- *   statistics (nb pers distinctes, nb pers vivantes, nb fam, ...)
+ * GenJ - ReportMultDesc TODO Daniel titles statistics (nb pers distinctes, nb
+ * pers vivantes, nb fam, ...)
  */
 public class ReportMultDesc extends Report {
+
+  private int nbColumns;
+
+  // Statistics
+  private int nbIndi = 0;
+
+  private int nbFam = 0;
+
+  private int nbLiving = 0;
+
+  private final static int ONE_LINE = 0, ONE_EVT_PER_LINE = 1;
+
+  public int reportFormat = ONE_LINE;
+
+  public String reportFormats[] = { translate("IndiPerLine"),
+      translate("EventPerLine") };
+
+  public int reportMaxGenerations = 999;
+
+  public boolean showAllPlaceJurisdictions = false;
+
+  public boolean reportPlaceOfBirth = true;
+
+  public boolean reportDateOfBirth = true;
+
+  public boolean reportPlaceOfDeath = true;
+
+  public boolean reportDateOfDeath = true;
+
+  public boolean reportPlaceOfMarriage = true;
+
+  public boolean reportDateOfMarriage = true;
+
+  public boolean reportPlaceOfOccu = true;
+
+  public boolean reportDateOfOccu = true;
+
+  public boolean reportPlaceOfResi = true;
+
+  public boolean reportDateOfResi = true;
+
+  public boolean reportMailingAddress = true;
+
+  // Privacy
+  public int publicGen = 0;
   
-    private Formatter output;
-    //    private String eol= System.getProperty("line.separator");
-    private int nbColumns;
-    // Statistics
-    private int nbIndi=0;
-    private int nbFam=0;
-    private int nbLiving=0;
+  /**
+   * don't need stdout
+   */
+  public boolean usesStandardOut() {
+    return false;
+  }
+  
+  /**
+   * use the fo image
+   */
+  protected ImageIcon getImage() {
+    return Report.IMG_FO;
+  }
 
-    private final static int
-	ONE_LINE = 0,
-	ONE_EVT_PER_LINE = 1;
-    public int reportFormat=ONE_LINE;
-    public String reportFormats[] = { translate("IndiPerLine"),
-				      translate("EventPerLine") };
+  /**
+   * Main for argument individual
+   */
+  public void start(Indi indi) {
+    start( new Indi[] { indi }, translate("title.descendant", indi.getName()));
+  }
 
-    private final static int
-	HTML = 0,
-	TEXT = 1,
-	TEXT_CSV = 2;
-    public int outputFormat=HTML;
-    public String outputFormats[] = { translate("Html")
-				      ,translate("Text")
-				      ,"Csv"
-    };
-
-    private int columnWidth = 30;
-    public int reportMaxGenerations = 999;
-
-    public boolean showAllPlaceJurisdictions = false;
-
-    public boolean reportPlaceOfBirth = true;
-    public boolean reportDateOfBirth = true;
-    public boolean reportPlaceOfDeath = true;
-    public boolean reportDateOfDeath = true;
-    public boolean reportPlaceOfMarriage = true;
-    public boolean reportDateOfMarriage = true;
-    public boolean reportPlaceOfOccu = true;
-    public boolean reportDateOfOccu = true;
-    public boolean reportPlaceOfResi = true;
-    public boolean reportDateOfResi = true;
-    public boolean reportMailingAddress = true;
-
-    // Privacy
-    public int publicGen = 0;
-
-    /**
-     * Main for argument individual
-     */
-    public void start(Indi indi) {
-	Indi[] indis = {indi};
-	start(indis);
-    }
   /**
    * One of the report's entry point
    */
-    public void start(Indi[] indis) {
-	// prepare our index
-	Map primary = new TreeMap();
+  public void start(Indi[] indis) {
+    start( indis, getName() + " - " + indis[0].getGedcom().getName());
+  }
+  
+  /**
+   * Our main private report point
+   */
+  private void start(Indi[] indis, String title) {
+    
+    // keep track of who we looked at already
+    Set done = new HashSet();
 
-	// Init some stuff
-
-	if (outputFormat == HTML) {
-	    output = new FormatterHtml(this);
-	    ((FormatterHtml) output).
-		setStyle("td.report{vertical-align:top;}"+
-			 "div.indent {margin-left:30px;"+
-			 "}"+
-			 "p{margin-top:0;margin-bottom:0;"+
-			 "text-indent: - 20px;"+
-			 //"padding-left:20px;"+
-			 "}"+
-			 "h2.report{border-color:black;background-color:#f0f0f0;border-style:solid;border-width:0 0 2 0;text-transform:uppercase;}");
-	}else if (outputFormat == TEXT){
-	    output = new FormatterText(this);
-	    ((FormatterText) output).setTabStop(new int[] {-7,
-							   7+columnWidth,
-							   7+columnWidth*2,
-							   7+columnWidth*3,
-							   7+columnWidth*4,
-							   7+columnWidth*5,
-							   7+columnWidth*6});
-	    ((FormatterText) output).setNiceColumns(false);
-	} else if (outputFormat == TEXT_CSV){
-	    output = new FormatterCsv(this);
-	}
-
+    // Init some stuff
     PrivacyPolicy policy = OPTIONS.getPrivacyPolicy();
 
-	nbColumns = 2;
-	if (reportPlaceOfBirth ||  reportDateOfBirth) nbColumns++;
-	if (reportPlaceOfMarriage || reportDateOfMarriage) nbColumns++;
-	if (reportPlaceOfDeath  || reportDateOfDeath) nbColumns++;
-	if (reportPlaceOfOccu || reportDateOfOccu) nbColumns++;
-	if (reportPlaceOfResi || reportDateOfResi) nbColumns++;
+    nbColumns = 2;
+    if (reportPlaceOfBirth || reportDateOfBirth)
+      nbColumns++;
+    if (reportPlaceOfMarriage || reportDateOfMarriage)
+      nbColumns++;
+    if (reportPlaceOfDeath || reportDateOfDeath)
+      nbColumns++;
+    if (reportPlaceOfOccu || reportDateOfOccu)
+      nbColumns++;
+    if (reportPlaceOfResi || reportDateOfResi)
+      nbColumns++;
 
-	output.start();
-	// iterate into individuals and all its descendants
-	for (int i = 0; i < indis.length; i++)
-	    iterate(indis[i], 1, new Integer(i+1).toString(), primary, policy);
-
-	output.println(output.h(1,translate("title.stats")));
-	output.println(translate("nb.fam", nbFam));
-	output.println(translate("nb.indi", nbIndi));
-	output.println(translate("nb.living", nbLiving));
- 
-	// Done
-	output.end();
-
-    }    
-
+    Document doc = new Document(title);
     
-private void iterate(Indi indi, int level, String num, Map primary, PrivacyPolicy policy) {
-    boolean addIndi=true;
+    // iterate into individuals and all its descendants
+    for (int i = 0; i < indis.length; i++) {
+      Indi indi = indis[i];
+      doc.startSection( translate("title.descendant", indi.getName()) );
+      iterate(indi, 1, done, policy, doc);
+    }
 
-    if (level > reportMaxGenerations) return;
+    doc.startSection( translate("title.stats") );
+    doc.addText( translate("nb.fam", nbFam) );
+    doc.nextParagraph();
+    doc.addText( translate("nb.indi", nbIndi) );
+    doc.nextParagraph();
+    doc.addText( translate("nb.living", nbLiving) );
+
+    // done
+    showDocumentToUser(doc);
+
+  }
+
+  /**
+   * Generate descendants information for one individual
+   */
+  private void iterate(Indi indi, int level, Set done, PrivacyPolicy policy, Document doc) {
     
+    done.add(indi);
+    nbIndi++;
+    if (indi!=null&&!indi.isDeceased()) nbLiving ++;
+
+    // no more?
+    if (level > reportMaxGenerations)
+      return;
+
     // still in a public generation?
-    PrivacyPolicy localPolicy = level<publicGen+1 ? PrivacyPolicy.PUBLIC : policy;
-    
-    // Here comes the individual
-    if (level == 1)
-	output.println(output.h(1,translate("title.descendant",indi.getName())));
+    PrivacyPolicy localPolicy = level < publicGen + 1 ? PrivacyPolicy.PUBLIC : policy;
 
-    output.println(format(indi, null, num, localPolicy));
-           
+    // format the indi's information
+    doc.startList();
+    format(indi, (Fam)null, "", localPolicy, doc);
+
     // And we loop through its families
     Fam[] fams = indi.getFamiliesWhereSpouse();
-    for (int f=0;f<fams.length;f++) {
-	// .. here's the fam and spouse
-	Fam fam = fams[f];
-	Indi spouse= fam.getOtherSpouse(indi);
-            
-	// .. a line for the spouse
-	if (fams.length==1)
-	    //	    output.println(format(spouse,fam,getIndent(level)+formatString("x",num.length()), level)); 
-	    output.println(format(spouse,fam,"x", localPolicy)); 
-	else 
-	    output.println(format(spouse,fam,"x"+(f+1), localPolicy)); 
-	//	    output.println(format(spouse,fam,getIndent(level)+formatString("x"+(f+1),num.length()), level)); 
-	
-	Object seeIndi = primary.get(fam.getId());
-	if (seeIndi != null){
-	    // don't add indi if already seens
-	    addIndi = false;
-	    output.startIndent();
-	    output.startIndent();
-	    output.println("====> "+translate("see")+output.hlink(seeIndi.toString(),seeIndi.toString()));
-	    output.endIndent();
-	    output.endIndent();
-	} else {
-	    nbFam++;
-	    if (spouse != null){
-		nbIndi++;
-		nbLiving += (spouse.getProperty("DEAT") == null)? 1 : 0;
-	    }
-	    primary.put( fam.getId(),num);
-            // .. and all the kids
-            Indi[] children = fam.getChildren();
-	    if (children.length !=0){
-		output.startIndent();
-	    }
-            for (int c = 0; c < children.length; c++) {
-		// do the recursive step
-		if (fams.length==1)
-		    iterate(children[c], level+1,num+'.'+(c+1),primary, policy);
-                else
-		    iterate(children[c], level+1,num+'x'+(f+1)+'.'+(c+1),primary, policy);
-                // .. next child
-            }
-	    if (children.length !=0){
-		output.endIndent();
-	    }
-	}
-     	// .. next family
-    }
-    if (addIndi){
-	nbIndi++;
-	nbLiving += (indi.getProperty("DEAT") == null)? 1 : 0;
-    }
-}
-    
-    /**
-     * resolves the information of one Indi
-     */
-  private String format(Indi indi, Fam fam, String num, PrivacyPolicy policy) {
+    for (int f = 0; f < fams.length; f++) {
+      
+      // .. here's the fam and spouse
+      Fam fam = fams[f];
+      Indi spouse = fam.getOtherSpouse(indi);
 
-	String number;
-	String name;
-	String birth;
-	String death;
-	String marriage;
-	String occupation;
-	String residence;
-	PropertyMultilineValue address = null;
+      // output the spouse
+    	if (fams.length==1)
+    	    format(spouse,fam,"x", localPolicy, doc); 
+    	else 
+    	    format(spouse,fam,"x"+(f+1), localPolicy, doc); 
 
-	String result = new String();
+      // put out a link if we've seen the spouse already
+      if (done.contains(spouse)) {
+        doc.nextParagraph();
+        doc.addText("====> " + translate("see") );
+        
+        // FIXME Nils re-enable links to already seen individuals
+        // doc.addLink( seeIndi.toString(), seeIndi.getId() );
+      } else {
 
-	// Might be null
-	if (indi==null) 
-	    return "?";
-  
-	number = ""+num;
-	number = output.anchor(number, number);
-	name = output.strong( policy.getDisplayValue(indi, "NAME") )+" ("+indi.getId()+")";
-	birth = format(indi, "BIRT", OPTIONS.getBirthSymbol(), reportDateOfBirth, reportPlaceOfBirth, policy);
-	if (fam != null){
-	    marriage = format(fam, "MARR", OPTIONS.getMarriageSymbol(),reportDateOfMarriage, reportPlaceOfMarriage, policy);
-	} else {
-	    marriage = "";
-	}
-	death = format(indi, "DEAT", OPTIONS.getDeathSymbol(), reportDateOfDeath, reportPlaceOfDeath, policy);
-	occupation = format(indi, "OCCU", "{$T}{ $V}", reportDateOfOccu, reportPlaceOfOccu, policy);
-	residence = format(indi, "RESI", "{$T}", reportDateOfResi, reportPlaceOfResi, policy);
-	if (reportMailingAddress) {
-	  address = indi.getAddress();
-      if (address!=null&&policy.isPrivate(address)) address = null;
-    }
-
-	if (outputFormat == TEXT_CSV) {
-	    String separator=" ";
-	    result = "";
-	    if (birth != null && birth.length() != 0){ 
-		result += separator + birth;
-		separator = "; ";
-	    }
-	    if (marriage != null && marriage.length() != 0){
-		result += separator + marriage;
-		separator = "; ";
-	    }
-	    if (death != null && death.length() != 0){
-		result += separator + death;
-		separator = "; ";
-	    }
-	    if (occupation != null && occupation.length() != 0){
-		result += separator + occupation;
-		separator = "; ";
-	    }
-	    if (residence != null && residence.length() != 0){
-		result += separator + residence;
-		separator = "; ";
-	    }
-	    result = output.cell(number)
-		+output.cell(name)
-		+output.cell(result);
-	    if (address != null) {
-	        String[] lines = address.getLines();
-    		for (int i = 0; i<lines.length; i++){
-    		    result += output.cell(lines[i]);
-    		}
+        done.add(spouse);
+        nbIndi++;
+        nbFam++;
+        if (spouse!=null&&!spouse.isDeceased()) nbLiving ++;
+        
+        // .. and all the kids
+        Indi[] children = fam.getChildren();
+        for (int c = 0; c < children.length; c++) {
+          // do the recursive step
+          if (fams.length == 1)
+            iterate(children[c], level + 1, done, policy, doc);
+          else
+            iterate(children[c], level + 1, done, policy, doc);
+          
+          // .. next child
         }
-	    result = output.row(result);
-	} else {
-	    if (reportFormat == ONE_LINE) {
-		String separator=" ";
-		result = output.underline(number);
-		result += " " + name;
-		if (birth != null && birth.length() != 0){ 
-		    result += separator + birth;
-		    separator = "; ";
-		}
-		if (marriage != null && marriage.length() != 0){
-		    result += separator + marriage;
-		    separator = "; ";
-		}
-		if (death != null && death.length() != 0){
-		    result += separator + death;
-		    separator = "; ";
-		}
-		if (occupation != null && occupation.length() != 0){
-		    result += separator + occupation;
-		    separator = "; ";
-		}
-		if (residence != null && residence.length() != 0){
-		    result += separator + residence;
-		    separator = "; ";
-		}
-	    } else {
-		result = "";
-		if (birth != null) result += output.li(birth);
-		if (marriage != null) result += output.li(marriage);
-		if (death != null) result += output.li(death);
-		if (occupation != null) result += output.li(occupation);
-		if (residence != null) result += output.li(residence);
-		
-		result = number+" "+name+output.ul(result);
-	    }
-	}
-    	return result;
+        
+      }
+      // .. next family
     }
+    
+    // done
+    doc.endList();
+  }
 
-  /** 
+  /**
+   * resolves the information of one Indi
+   */
+  private void format(Indi indi, Fam fam, String prefix, PrivacyPolicy policy, Document doc) {
+
+    // Might be null
+    if (indi == null)
+      return;
+
+    // FIXME Nils re-enable anchors for individuals processes
+    
+    doc.nextParagraph();
+    doc.addText( policy.getDisplayValue(indi, "NAME") + " (" + indi.getId() + ")" );
+    
+    String birt = format(indi, "BIRT", OPTIONS.getBirthSymbol(), reportDateOfBirth, reportPlaceOfBirth, policy);
+    String marr = fam!=null ? format(fam, "MARR", OPTIONS.getMarriageSymbol(), reportDateOfMarriage, reportPlaceOfMarriage, policy) : "";
+    String deat = format(indi, "DEAT", OPTIONS.getDeathSymbol(), reportDateOfDeath, reportPlaceOfDeath, policy);
+    String occu = format(indi, "OCCU", "{$T}{ $V}", reportDateOfOccu, reportPlaceOfOccu, policy);
+    String resi = format(indi, "RESI", "{$T}", reportDateOfResi, reportPlaceOfResi, policy);
+    PropertyMultilineValue addr = reportMailingAddress ? indi.getAddress() : null;
+    if (addr != null && policy.isPrivate(addr)) addr = null;
+
+//    if (outputFormat == TEXT_CSV) {
+//      String separator = " ";
+//      result = "";
+//      if (birth != null && birth.length() != 0) {
+//        result += separator + birth;
+//        separator = "; ";
+//      }
+//      if (marriage != null && marriage.length() != 0) {
+//        result += separator + marriage;
+//        separator = "; ";
+//      }
+//      if (death != null && death.length() != 0) {
+//        result += separator + death;
+//        separator = "; ";
+//      }
+//      if (occupation != null && occupation.length() != 0) {
+//        result += separator + occupation;
+//        separator = "; ";
+//      }
+//      if (residence != null && residence.length() != 0) {
+//        result += separator + residence;
+//        separator = "; ";
+//      }
+//      result = output.cell(number) + output.cell(name) + output.cell(result);
+//      if (address != null) {
+//        String[] lines = address.getLines();
+//        for (int i = 0; i < lines.length; i++) {
+//          result += output.cell(lines[i]);
+//        }
+//      }
+//      result = output.row(result);
+//    } 
+    
+    // dump the information
+    if (reportFormat!=ONE_LINE) 
+      doc.startList();
+    
+    String[] infos = new String[] { birt, marr, deat, occu, resi };
+    for (int i=0, j=0; i<infos.length ; i++) {
+      if (infos[i].length()==0)
+        continue;
+      if (++j>1) {
+        if (reportFormat==ONE_LINE)  doc.addText(", ");
+        else doc.nextListItem();
+      }
+      doc.addText(infos[i]);
+    }
+    
+    if (reportFormat!=ONE_LINE) 
+      doc.endList();
+    
+    // done
+  }
+  
+  /**
    * convert given prefix, date and place switches into a format string
    */
   private String format(Entity e, String tag, String prefix, boolean date, boolean place, PrivacyPolicy policy) {
+    
     Property prop = e.getProperty(tag);
-    if (prop==null)
+    if (prop == null)
       return "";
-    
-    String format = prefix + (date?"{ $D}":"")
-      +(place&&showAllPlaceJurisdictions ? "{ $P}" : "")
-      +(place&&!showAllPlaceJurisdictions ? "{ $p}" : "");
-    
+
+    String format = prefix + (date ? "{ $D}" : "")
+        + (place && showAllPlaceJurisdictions ? "{ $P}" : "")
+        + (place && !showAllPlaceJurisdictions ? "{ $p}" : "");
+
     return prop.format(format, policy);
 
   }
-  
 
-} //ReportMulDesv
+} // ReportMulDesv
