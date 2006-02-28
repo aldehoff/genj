af28d4a34f931ebc80be00280fb58dd44d7118b9
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2006-02-28 16:58:21 +0000

    geo: added testcase for location parsing
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@4001 fbd980a2-0dff-11df-b4b7-0b05b74d0985

---
+++ geo/src/tst/genj/geo/GeoLocationTest.java
@@ -0,0 +1,104 @@
+/**
+ * GenJ - GenealogyJ
+ *
+ * Copyright (C) 1997 - 2005 Nils Meier <nils@meiers.net>
+ *
+ * This piece of code is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package genj.geo;
+
+import genj.gedcom.Gedcom;
+import genj.gedcom.Indi;
+import genj.gedcom.Property;
+import genj.util.Origin;
+
+import java.util.Locale;
+
+import junit.framework.TestCase;
+
+/**
+ * Test Geo Location functionality
+ */
+public class GeoLocationTest extends TestCase {
+
+  private Indi indi;
+  
+  /**
+   * Prepare a fake indi
+   */
+  protected void setUp() throws Exception {
+
+    // create gedcom
+    Gedcom gedcom = new Gedcom(Origin.create("file://foo.ged"));
+    
+    // and individual
+    indi = (Indi)gedcom.createEntity(Gedcom.INDI);
+    
+  }
+  
+  /**
+   * location parsing
+   */
+  public void testParsing() {
+
+    Locale.setDefault(Locale.GERMAN);
+    
+    // these should all be the same
+    GeoLocation[] locations = {
+      locate(place("Timaru, Neuseeland")),
+      locate(place("Timaru, New Zealand")),
+      locate(place("Timaru, nz")),
+      locate(addr("Timaru", "New Zealand")),
+      locate(addr("Timaru", "Neuseeland")),
+      locate(addr("Timaru", "nz"))
+    };
+    
+    for (int l = 1; l < locations.length; l++) {
+      assertEquals(locations[l-1], locations[l]);
+    }
+    
+    // but they shouldn't be the same as this one
+    GeoLocation other = locate(place("Timaru"));
+    for (int l = 0; l < locations.length; l++) {
+      // interesting - use Object cast otherwise GeoLocation's super.equals() is called
+      assertFalse(other.equals((Object)locations[l])); 
+    }
+    
+
+    // done
+  }
+  
+  private GeoLocation locate(Property prop) {
+    return new GeoLocation(prop);
+  }
+
+  private Property addr(String city, String country) {
+    
+    Property event = indi.addProperty("EVEN", "");
+    
+    Property addr = event.addProperty("ADDR", "");
+    addr.addProperty("CITY", city);
+    addr.addProperty("CTRY", country);
+    
+    return event;
+  }
+    
+  private Property place(String value) {
+    Property event = indi.addProperty("EVEN", "");
+    event.addProperty("PLAC", value);
+    return event;
+  }
+
+}
