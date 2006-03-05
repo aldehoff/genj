77bf7da6384b1da23f87e1d0e635f691825c9da2
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2006-03-05 03:33:00 +0000

    tst: added a test-case for origin's calculations
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@4055 fbd980a2-0dff-11df-b4b7-0b05b74d0985

---
+++ genj/src/tst/genj/util/OriginTest.java
@@ -0,0 +1,36 @@
+/**
+ * JUNIT TESTCASE - DONT PACKAGE FOR DISTRIBUTION
+ */
+package genj.util;
+
+import java.io.File;
+
+import junit.framework.TestCase;
+
+/**
+ * Test origin functionality
+ */
+public class OriginTest extends TestCase {
+
+  /**
+   * test origin's way of calculating relative paths
+   */
+  public void testRelative() throws Throwable {
+    
+    String dir = "./gedcom";
+    Origin origin = Origin.create(new File(dir, "example.ged").toURL());
+
+    assertEquals(null, origin.calcRelativeLocation("foo.jpg"));
+    assertEquals("foo.jpg", origin.calcRelativeLocation(new File(dir, "foo.jpg").toString()));
+    assertEquals("foo.jpg", origin.calcRelativeLocation(new File(dir, "../gedcom/foo.jpg").toString()));
+    assertEquals("foo.jpg", origin.calcRelativeLocation(new File(dir, "./foo.jpg").toString()));
+    assertEquals(null, origin.calcRelativeLocation("/foo.jpg"));
+    
+    assertEquals("question marks are no good", origin.calcRelativeLocation(new File(dir, "question marks are no good").toString()));
+    assertEquals(null, origin.calcRelativeLocation(new File(dir, "right?").toString()));
+    assertEquals(null, origin.calcRelativeLocation("right?"));
+    
+    
+  }
+  
+}
