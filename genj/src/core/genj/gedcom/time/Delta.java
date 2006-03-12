2c290a4a4d319efbd0a38a81fc8b00a270f48bf8
pewu <pewu@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2006-03-12 01:06:44 +0000

    lang: now translations do not need to translate unnecessary time.dayss, monthss, yearss (used for Slavonic plurals)
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@4168 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/src/core/genj/gedcom/time/Delta.java
+++ genj/src/core/genj/gedcom/time/Delta.java
@@ -30,15 +30,41 @@ public class Delta implements Comparable {
 
   /** localizations */
   public final static String
-    TXT_YEAR    = PointInTime.resources.getString("time.year"   ),
-    TXT_YEARS   = PointInTime.resources.getString("time.years"  ),
-    TXT_YEARSS  = PointInTime.resources.getString("time.yearss" ),
-    TXT_MONTH   = PointInTime.resources.getString("time.month"  ),
-    TXT_MONTHS  = PointInTime.resources.getString("time.months" ),
-    TXT_MONTHSS = PointInTime.resources.getString("time.monthss"),
-    TXT_DAY     = PointInTime.resources.getString("time.day"    ),
-    TXT_DAYS    = PointInTime.resources.getString("time.days"   ),
-    TXT_DAYSS   = PointInTime.resources.getString("time.dayss"  );
+    TXT_DAY     = PointInTime.resources.getString("time.day"   ),
+    TXT_DAYS    = PointInTime.resources.getString("time.days"  ),
+    TXT_DAYSS,
+    TXT_MONTH   = PointInTime.resources.getString("time.month" ),
+    TXT_MONTHS  = PointInTime.resources.getString("time.months"),
+    TXT_MONTHSS,
+    TXT_YEAR    = PointInTime.resources.getString("time.year"  ),
+    TXT_YEARS   = PointInTime.resources.getString("time.years" ),
+    TXT_YEARSS;
+
+  /**
+   * Load values for Slavonic plural forms. If they don't exist, use normal plural.
+   */
+  static {
+      // dayss
+      String ss = PointInTime.resources.getString("time.dayss");
+      if (ss.equals("time.dayss"))
+          TXT_DAYSS = PointInTime.resources.getString("time.days");
+      else
+          TXT_DAYSS = ss;
+
+      // monthss
+      ss = PointInTime.resources.getString("time.monthss");
+      if (ss.equals("time.monthss"))
+          TXT_MONTHSS = PointInTime.resources.getString("time.months");
+      else
+          TXT_MONTHSS = ss;
+
+      // yearss
+      ss = PointInTime.resources.getString("time.yearss");
+      if (ss.equals("time.yearss"))
+          TXT_YEARSS = PointInTime.resources.getString("time.years");
+      else
+          TXT_YEARSS = ss;
+  }
 
   /** values */
   private int years, months, days;
