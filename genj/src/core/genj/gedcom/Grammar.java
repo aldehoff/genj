bb6e2e9053444b29a618fca76bf35d1182e4cccc
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2006-06-13 13:36:32 +0000

    core: improved error logging in case of problems with loading grammar
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@4392 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/src/core/genj/gedcom/Grammar.java
+++ genj/src/core/genj/gedcom/Grammar.java
@@ -29,6 +29,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Stack;
 import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
@@ -47,6 +48,8 @@ public class Grammar {
   
   /** meta roots */
   private Map tag2root = new HashMap();
+  
+  private final static Logger LOG = Logger.getLogger("genj.gedcom");
   
   /**
    * Singleton Constructor
@@ -190,7 +193,13 @@ public class Grammar {
         properties.put(attributes.getQName(i), attributes.getValue(i));
       
       // create a meta property for element
-      MetaProperty meta = new MetaProperty(Grammar.this, qName, properties, true);
+      MetaProperty meta;
+      try {
+        meta = new MetaProperty(Grammar.this, qName, properties, true);
+      } catch (Throwable t) {
+        LOG.log(Level.SEVERE, "Problem instantiating meta property for "+qName+" with "+properties, t.getCause()!=null?t.getCause():t);
+        throw new Error("Can't parse Gedcom Grammar");
+      }
 
       // a property root (a.k.a entity) or a nested one?
       if (stack.isEmpty())  {
