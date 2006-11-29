ff688970a85d64830e03f552449c47ed9cf7ae13
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2006-11-29 18:47:39 +0000

    build: no need for ant.jar in module dev itself (we can use the one in dev/app)
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@4556 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- packaging/build.cmd
+++ packaging/build.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 
-set classpath=%JAVA_HOME%/lib/tools.jar;./contrib/ant/ant.jar
+set classpath=%JAVA_HOME%/lib/tools.jar;./app/contrib/ant/ant.jar
 "%JAVA_HOME%\bin\java" org.apache.tools.ant.Main %1 %2 %3 %4
 
 endlocal
--- packaging/build.sh
+++ packaging/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #
 
-$JAVA_HOME/bin/java -cp $JAVA_HOME/lib/tools.jar:./contrib/ant/ant.jar org.apache.tools.ant.Main $1 $2 $3
+$JAVA_HOME/bin/java -cp $JAVA_HOME/lib/tools.jar:./app/contrib/ant/ant.jar org.apache.tools.ant.Main $1 $2 $3
 
