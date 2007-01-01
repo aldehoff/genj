c17c65fd5c1c7119c8c46436c49a197d786238d9
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2007-01-01 22:41:19 +0000

    bug: fix from sven for running proxies successfully under jdk 1.6 (as reported by Francois)
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@4584 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/src/contrib/spin/JDKProxyFactory.java
+++ genj/src/contrib/spin/JDKProxyFactory.java
@@ -20,8 +20,8 @@ package spin;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.lang.reflect.Proxy;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -30,76 +30,115 @@ import java.util.Set;
  */
 public class JDKProxyFactory extends ProxyFactory {
 
-    public Object createProxy(Object object, Evaluator evaluator) {
-        Class clazz = object.getClass();
-        return Proxy.newProxyInstance(clazz.getClassLoader(),
-                                      interfaces(clazz),
-                                      new SpinInvocationHandler(object, evaluator));
+  /**
+   * The created proxy will not implement non-public interfaces from different
+   * class loaders since these yield an {@link IllegalAccessError} on
+   * construction of a proxy.
+   */
+  public Object createProxy(Object object, Evaluator evaluator) {
+    Class clazz = object.getClass();
+
+    return Proxy.newProxyInstance(clazz.getClassLoader(),
+        getAccessibleInterfaces(clazz), new SpinInvocationHandler(
+            object, evaluator));
+  }
+
+  /**
+   * Utility method to retrieve all accessible interfaces of a class and its
+   * superclasses.
+   * 
+   * @param clazz
+   *            class to get interfaces for
+   * @return implemented interfaces
+   */
+  private static Class[] getAccessibleInterfaces(Class clazz) {
+    ClassLoader loader = clazz.getClassLoader();
+
+    Set interfaces = new HashSet();
+    while (clazz != null) {
+      Class[] candidates = clazz.getInterfaces();
+      for (int c = 0; c < candidates.length; c++) {
+        Class candidate = candidates[c];
+
+        if (!Modifier.isPublic(candidate.getModifiers())) {
+          if (candidate.getClassLoader() != loader) {
+            // see note
+            continue;
+          }
+        }
+
+        interfaces.add(candidate);
+      }
+
+      clazz = clazz.getSuperclass();
     }
-    
+
+    return (Class[]) interfaces.toArray(new Class[interfaces.size()]);
+  }
+
+  public boolean isProxy(Object object) {
+
+    if (object == null) {
+      return false;
+    }
+
+    if (!Proxy.isProxyClass(object.getClass())) {
+      return false;
+    }
+
+    return (Proxy.getInvocationHandler(object) instanceof SpinInvocationHandler);
+  }
+
+  protected boolean areProxyEqual(Object proxy1, Object proxy2) {
+
+    SpinInvocationHandler handler1 = (SpinInvocationHandler) Proxy
+        .getInvocationHandler(proxy1);
+    SpinInvocationHandler handler2 = (SpinInvocationHandler) Proxy
+        .getInvocationHandler(proxy2);
+
+    return handler1.object.equals(handler2.object);
+  }
+
+  /**
+   * Invocation handler for the <em>Spin</em> proxy.
+   */
+  private class SpinInvocationHandler implements InvocationHandler {
+
+    private Object object;
+
+    private Evaluator evaluator;
+
     /**
-     * Utility method to retrieve all implemented interfaces of a class
-     * and its superclasses.
+     * Create a new handler of invocations.
      * 
-     * @param clazz   class to get interfaces for
-     * @return        implemented interfaces
+     * @param object
+     *            the object to invoke methods on
+     * @param evaluator
+     *            the evaluator of methods
      */
-    private static Class[] interfaces(Class clazz) {
-      Set interfaces = new HashSet();
-      while (clazz != null) {
-        interfaces.addAll(Arrays.asList(clazz.getInterfaces()));
-        clazz = clazz.getSuperclass();
-      }
-
-      return (Class[])interfaces.toArray(new Class[interfaces.size()]);
+    public SpinInvocationHandler(Object object, Evaluator evaluator) {
+      this.object = object;
+      this.evaluator = evaluator;
     }
 
-    public boolean isProxy(Object object) {
-      
-      if (object == null) {
-          return false;
-      }
-      
-      if (!Proxy.isProxyClass(object.getClass())) {
-          return false;
-      }
-      
-      return (Proxy.getInvocationHandler(object) instanceof SpinInvocationHandler); 
-    }
-    
-    protected boolean areProxyEqual(Object proxy1, Object proxy2) {
-        
-        SpinInvocationHandler handler1 = (SpinInvocationHandler)Proxy.getInvocationHandler(proxy1); 
-        SpinInvocationHandler handler2 = (SpinInvocationHandler)Proxy.getInvocationHandler(proxy2);
-        
-        return handler1.object.equals(handler2.object); 
-    }
-    
     /**
-     * Invocation handler for the <em>Spin</em> proxy.
+     * Handle the invocation of a method on the <em>Spin</em> proxy.
+     * 
+     * @param proxy
+     *            the proxy instance
+     * @param method
+     *            the method to invoke
+     * @param args
+     *            the arguments for the method
+     * @return the result of the invocation on the wrapped object
+     * @throws Throwable
+     *             if the wrapped method throws a <code>Throwable</code>
      */
-    private class SpinInvocationHandler implements InvocationHandler {
+    public Object invoke(Object proxy, Method method, Object[] args)
+        throws Throwable {
 
-      private Object object;
-      private Evaluator evaluator;
-      
-      public SpinInvocationHandler(Object object, Evaluator evaluator) {
-          this.object  = object;
-          this.evaluator = evaluator;
-      }
-      
-      /**
-       * Handle the invocation of a method on the <em>Spin</em> proxy.
-       *
-       * @param proxy      the proxy instance
-       * @param method     the method to invoke
-       * @param args       the arguments for the method
-       * @return           the result of the invocation on the wrapped object
-       * @throws Throwable if the wrapped method throws a <code>Throwable</code>
-       */
-      public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-          
-        return evaluteInvocation(evaluator, proxy, new Invocation(this.object, method, args));
-      }
-    } 
+      return evaluteInvocation(evaluator, proxy, new Invocation(
+          this.object, method, args));
+    }
+  }
 }
