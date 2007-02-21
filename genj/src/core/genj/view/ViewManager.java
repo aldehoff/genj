cf2464167a8c7871b0c017a2ac16e04229111bd0
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2007-02-21 02:26:09 +0000

    ui: test - faking the left mouse click has actually always been a little bit of a hack-to-far - let's see what people say if they have to left-click themselves before right-clicking to change the current selection - this makes opening a context menu on a multi-selection more intuitive though
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@4743 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/src/core/genj/view/ViewManager.java
+++ genj/src/core/genj/view/ViewManager.java
@@ -739,21 +739,7 @@ public class ViewManager {
       if (!(component instanceof JComponent))
         return;
       JComponent jcomponent = (JComponent)component;
-      
-      // fake a normal click event so that popup trigger does a selection as well as non-popup trigger
-      if (!EnvironmentChecker.isMac()&&!me.isControlDown() && !me.isShiftDown() && me.getButton()!=MouseEvent.BUTTON1) {
-        MouseListener[] ms = me.getComponent().getMouseListeners();
-        MouseEvent fake = new MouseEvent(me.getComponent(), me.getID(), me.getWhen(), 0, me.getX(), me.getY(), me.getClickCount(), false, MouseEvent.BUTTON1);
-        for (int m = 0; m < ms.length; m++)  {
-          switch (me.getID()) {
-            case MouseEvent.MOUSE_PRESSED:
-              ms[m].mousePressed(fake); break;
-            case MouseEvent.MOUSE_RELEASED:
-              ms[m].mouseReleased(fake); break;
-          }
-        }
-      }
-      
+
       // we're interested in popup trigger and double-click 
       boolean isDoubleClick = me.getID()==MouseEvent.MOUSE_CLICKED&&me.getClickCount()>1;
       if(!me.isPopupTrigger()&&!isDoubleClick)  
