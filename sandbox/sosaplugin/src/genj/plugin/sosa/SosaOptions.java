7f2fd58ca5a9fc559381a4ffc9fac7cbdcf82b83
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2008-01-11 13:53:47 +0000

    plugin:sosa:checked in first stab
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@5079 fbd980a2-0dff-11df-b4b7-0b05b74d0985

---
+++ sandbox/sosaplugin/src/genj/plugin/sosa/SosaOptions.java
@@ -0,0 +1,69 @@
+/**
+ * GenJ - GenealogyJ
+ *
+ * Copyright (C) 1997 - 2002 Nils Meier <nils@meiers.net>
+ *
+ * This piece of code is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * $Revision$ $Author$ $Date$
+ */
+package genj.plugin.sosa;
+
+import genj.option.OptionProvider;
+import genj.option.PropertyOption;
+
+import java.util.List;
+
+/** 
+ * Options for plugin package 
+ */
+public class SosaOptions extends OptionProvider {
+
+	/** 
+	 * THE instance of our _sosa_ options - we have to make 
+	 * sure it's actually the SosaOptions - not gedcom options 
+	 * we keep one instance of here 
+	 */
+	private static SosaOptions instance = new SosaOptions();
+
+	/** Sosa extension flag */
+	public boolean isExtendSosaIndexation = true;
+
+	// we don't need a method for returning the flag - we make 
+	// it a public attribute for now - if that needs changing we'll 
+	// have to provide a setter setExtendSosaIndexationFlag() 
+	// as well though 
+	// 
+	//   public boolean getExtendSosaIndexationFlag() { 
+	//        return extendSosaIndexationFlag; 
+	//    } 
+
+	/** 
+	 * callback - provide options during system init 
+	 */
+	public List getOptions() {
+
+		// introspect for options 
+		return PropertyOption.introspect(getInstance());
+	}
+
+	/** 
+	 * accessor - singleton instance 
+	 */
+	public static SosaOptions getInstance() {
+		return instance;
+	}
+
+} 
