c7a6fa2515ce03b38173a1cd870fe1a8553ffbde
pewu <pewu@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2008-05-09 22:28:21 +0000

    report: 'Hand tool' panning in screen output (submitted by Marcin Floryan)
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@5218 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/src/report/tree/graphics/ScreenOutput.java
+++ genj/src/report/tree/graphics/ScreenOutput.java
@@ -10,11 +10,17 @@ package tree.graphics;
 
 import genj.report.Report;
 
+import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.RenderingHints;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JComponent;
+import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 
 /**
@@ -33,19 +39,51 @@ public class ScreenOutput extends JScrollPane implements GraphicsOutput {
      * The component containing the whole tree view.
      */
     private JComponent view;
-
+    
+    private Point lastPoint;
+    
     /**
      * Constructs the object.
      */
     public ScreenOutput() {
         view = new JComponent() {
             public void paint(Graphics g) {
-                renderer.render((Graphics2D)g);
+            	Graphics2D g2 = (Graphics2D)g;
+            	g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+            			RenderingHints.VALUE_ANTIALIAS_ON);
+                renderer.render(g2);
             }
         };
 
         setViewportView(view);
         setPreferredSize(new Dimension(300, 200));
+        
+        addMouseListener(new MouseAdapter() {
+        	public void mousePressed(MouseEvent e) {
+        		lastPoint = e.getPoint();
+        		setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        	}
+        	
+        	public void mouseReleased(MouseEvent e) {
+        		setCursor(Cursor.getDefaultCursor());
+        	}
+        });
+        
+        addMouseMotionListener(new MouseAdapter() {
+  
+      		private JScrollBar hSb = getHorizontalScrollBar();
+    		private JScrollBar vSb = getVerticalScrollBar();
+        	
+        	public void mouseDragged(MouseEvent e) {
+          		int dX = lastPoint.x - e.getX();
+        		int dY = lastPoint.y - e.getY();
+        		
+        		hSb.setValue(hSb.getValue()+dX);
+        		vSb.setValue(vSb.getValue()+dY);
+        		lastPoint = e.getPoint();
+        	}
+        	
+        });
     }
 
     /**
@@ -55,6 +93,7 @@ public class ScreenOutput extends JScrollPane implements GraphicsOutput {
         this.renderer = renderer;
         view.setPreferredSize(new Dimension(renderer.getImageWidth(),
                 renderer.getImageHeight()));
+        
 
         /* This was supposed to center both scrollbars but it doesn't work
         JScrollBar sb = getHorizontalScrollBar();
