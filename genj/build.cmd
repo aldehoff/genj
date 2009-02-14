58453acd842dffcbc6b566660a22fa5d0b6423d2
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2009-02-14 23:28:47 +0000

    build: make sure to work with space in java_home path - use FINE for run
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@5323 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/build.cmd
+++ genj/build.cmd
@@ -2,7 +2,10 @@
 setlocal
 
 set classpath=%JAVA_HOME%/lib/tools.jar;./contrib/ant/ant.jar
-%JAVA_HOME%\bin\java org.apache.tools.ant.Main %1 %2 %3 %4
+
+echo Classpath is %classpath%
+
+"%JAVA_HOME%\bin\java" org.apache.tools.ant.Main %1 %2 %3 %4
 
 endlocal
 
