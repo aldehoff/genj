052ab22d2ec0af160490c2a6c4cbdfd55e449efd
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2009-02-28 17:44:14 +0000

    correctly handle size of vertices and use dist/2 in between dummies
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@5364 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- graphj/src/layouts/gj/layout/hierarchical/HierarchicalLayoutAlgorithm.java
+++ graphj/src/layouts/gj/layout/hierarchical/HierarchicalLayoutAlgorithm.java
@@ -19,9 +19,12 @@
  */
 package gj.layout.hierarchical;
 
+import static gj.geom.Geometry.getBounds;
+
 import gj.layout.Layout2D;
 import gj.layout.LayoutAlgorithm;
 import gj.layout.LayoutAlgorithmException;
+import gj.layout.hierarchical.LayerAssignment.DummyVertex;
 import gj.model.Edge;
 import gj.model.Graph;
 import gj.model.Vertex;
@@ -40,8 +43,8 @@ import java.util.List;
  */
 public class HierarchicalLayoutAlgorithm implements LayoutAlgorithm {
 
-  private double distanceBetweenLayers = 50; 
-  private double distanceBetweenVertices= 50; 
+  private double distanceBetweenLayers = 20; 
+  private double distanceBetweenVertices= 20; 
   private boolean isSinksAtBottom = true;
   private double alignmentOfLayers = 0.5;
   
@@ -65,60 +68,67 @@ public class HierarchicalLayoutAlgorithm implements LayoutAlgorithm {
     new LayerByLayerSweepCR().reduceCrossings(layerAssignment);
     
     // 3rd step - vertex positioning
-    assignPositions(layerAssignment, layout);
+    bounds = assignPositions(layerAssignment, layout);
     
     // 4th step - edge positioning
     routeEdges(graph, layerAssignment, layout);
     
     // done
-    return new Rectangle2D.Double(
-        -distanceBetweenVertices/2,
-        isSinksAtBottom?-layerAssignment.getHeight()*distanceBetweenLayers+distanceBetweenLayers/2:0,
-        layerAssignment.getWidth()*distanceBetweenVertices,
-        layerAssignment.getHeight()*distanceBetweenLayers
-    );
+    return bounds;
   }
 
   /**
    * assign positions to vertices
    */
-  private void assignPositions(LayerAssignment layerAssignment, Layout2D layout) {
+  private Rectangle2D assignPositions(LayerAssignment layerAssignment, Layout2D layout) {
     
-    int height = layerAssignment.getHeight();
+    int layers = layerAssignment.getHeight();
     
     // calculate true width of widest layer in points
-    double width = 0;
-    double[] widths = new double[height];
-    for (int i=0;i<height;i++) {
+    double totalHeight = 0;
+    double totalWidth = 0;
+    double[] layerWidths = new double[layers];
+    double[] layerHeights = new double[layers];
+    Rectangle2D[][] vertexBounds = new Rectangle2D[layers][layerAssignment.getWidth()];
+    for (int i=0;i<layers;i++) {
       for (int j=0;j<layerAssignment.getWidth(i);j++) {
-        if (j>0) widths[i]+=distanceBetweenVertices;
-        Vertex v = layerAssignment.getVertex(i, j);
-        widths[i] += layout.getShapeOfVertex(v).getBounds2D().getWidth();
+        if (j>0) layerWidths[i]+=distanceBetweenVertices;
+        Rectangle2D r = getBounds(layout.getShapeOfVertex(layerAssignment.getVertex(i, j)));
+        vertexBounds[i][j] = r;
+        layerWidths[i] += r.getWidth();
+        layerHeights[i] = Math.max(layerHeights[i], r.getHeight());
       }
-      if (widths[i]>width) width = widths[i];
+      totalHeight += layerHeights[i];
+      if (layerWidths[i]>totalWidth) totalWidth = layerWidths[i];
     }
-
-    // loop over layers and place vertices (not dummies of course)
+    
+    // loop over layers and place vertices 
     int dir = isSinksAtBottom ? -1 : 1;
-    for (int i=0;i<height;i++) {
-      double alignment = (width-layerAssignment.getWidth(i)*distanceBetweenVertices)*alignmentOfLayers; 
+    double y = 0;
+    for (int i=0;i<layers;i++) {
+      if (i>0) y += dir*distanceBetweenLayers;
+      double x = (totalWidth-layerWidths[i])*alignmentOfLayers;
       for (int j=0; j<layerAssignment.getWidth(i); j++) {
         Vertex vertex = layerAssignment.getVertex(i,j);
-        Point2D start = new Point2D.Double(alignment+j*distanceBetweenVertices, dir*i*distanceBetweenLayers);
-        layout.setPositionOfVertex(vertex, start);
+        if (j>0) x += (vertex instanceof DummyVertex || layerAssignment.getVertex(i,j-1) instanceof DummyVertex) ? distanceBetweenVertices/2 : distanceBetweenVertices;
+        Rectangle2D r = vertexBounds[i][j];
+        if (dir<0)
+          layout.setPositionOfVertex(vertex, new Point2D.Double(x - r.getMinX(), y - r.getMaxY() - (layerHeights[i]-r.getHeight())/2 ));
+        else
+          layout.setPositionOfVertex(vertex, new Point2D.Double(x - r.getMinX(), y - r.getMinY() + (layerHeights[i]-r.getHeight())/2 ));
+        x += r.getWidth();
       }
+      y += dir*layerHeights[i];
     }
     
     // done
+    return new Rectangle2D.Double(0,y<0?y:0,totalWidth,y<0?-y:y);
   }
 
   /**
    * route edges appropriately
    */
   private void routeEdges(Graph graph, LayerAssignment layerAssignment, Layout2D layout) {
-    
-    int width = layerAssignment.getWidth();
-    int dir = isSinksAtBottom ? -1 : 1;
     
     for (Edge edge : graph.getEdges()) {
       
