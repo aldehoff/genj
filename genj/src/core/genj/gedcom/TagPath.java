0fae5361ff440efbafb2f637ab66542a035f395f
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2010-01-26 01:40:11 +0000

    ui: don't let path tree be collapsed at root
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@5924 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/src/core/genj/gedcom/TagPath.java
+++ genj/src/core/genj/gedcom/TagPath.java
@@ -21,7 +21,6 @@ package genj.gedcom;
 
 import java.util.Collection;
 import java.util.Stack;
-import java.util.StringTokenizer;
 import java.util.regex.Pattern;
 
 /**
@@ -62,20 +61,13 @@ public class TagPath {
     this(path, null);
   }
   
-  /**
-   * Constructor for TagPath
-   * @param path path as colon separated string value a:b:c
-   * @exception IllegalArgumentException in case format isn't o.k.
-   */
-  public TagPath(String path, String name) throws IllegalArgumentException {
-    
+  public TagPath(String[] path, String name) throws IllegalArgumentException {
     // keep name
     this.name = name;
 
     // Parse path
-    StringTokenizer tokens = new StringTokenizer(path,SEPARATOR_STRING,false);
-    len = tokens.countTokens();
-    if (len==0||path.charAt(0)==SEPARATOR||path.charAt(path.length()-1)==SEPARATOR)
+    len = path.length;
+    if (len==0)
       throw new IllegalArgumentException("No valid path '"+path+"'");
 
     // ... setup data
@@ -84,7 +76,7 @@ public class TagPath {
     for (int i=0;i<len;i++) {
       
       // next token
-      String tag = tokens.nextToken().trim();
+      String tag = path[i];
       if (tag.length()==0) 
         throw new IllegalArgumentException("Empty tag in '"+path+"' is not valid");
 
@@ -94,6 +86,15 @@ public class TagPath {
     }
     
     // Done
+  }
+  
+  /**
+   * Constructor for TagPath
+   * @param path path as colon separated string value a:b:c
+   * @exception IllegalArgumentException in case format isn't o.k.
+   */
+  public TagPath(String path, String name) throws IllegalArgumentException {
+    this(path.split(SEPARATOR_STRING), name);
   }
   
   private void set(int pos, String tag) {
