3865b5a41432bb8793e2f38498ef370de4a23374
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2010-02-06 03:09:39 +0000

    ui: render multi line text
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@6056 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/src/core/genj/util/swing/GraphicsHelper.java
+++ genj/src/core/genj/util/swing/GraphicsHelper.java
@@ -33,19 +33,27 @@ public class GraphicsHelper {
   public static void render(Graphics2D graphics, String str, Rectangle2D box, double xalign, double yalign) {
     
     FontMetrics fm = graphics.getFontMetrics();
-    Rectangle2D r = fm.getStringBounds(str, graphics);
     LineMetrics lm = fm.getLineMetrics(str, graphics);
-    
-    float
-      w = (float)r.getWidth(),
-      h = (float)r.getHeight();
-      
-    double x = Math.max(box.getX(), box.getCenterX() - w*xalign);
-    double y = Math.max(box.getY(), box.getCenterY() - h*yalign + h - lm.getDescent()); 
-    
+
+    float h = 0;
+    String[] lines = str.split("\\\n");
+    float[] ws = new float[lines.length];
+    for (int i=0;i<lines.length;i++) {
+      Rectangle2D r = fm.getStringBounds(lines[i], graphics);
+      ws[i] = (float)r.getWidth();
+      h = Math.max(h,(float)r.getHeight());
+    }
+
     Shape clip = graphics.getClip();
     graphics.clip(box);
-    graphics.drawString(str, (float)x, (float)y);
+    
+    for (int i=0;i<lines.length;i++) {
+      double x = Math.max(box.getX(), box.getCenterX() - ws[i]*xalign);
+      double y = Math.max(box.getY(), box.getY() + (box.getHeight()-lines.length*h)*yalign + i*h + h - lm.getDescent()); 
+      
+      graphics.drawString(lines[i], (float)x, (float)y);
+    }
+    
     graphics.setClip(clip);
   }
   
