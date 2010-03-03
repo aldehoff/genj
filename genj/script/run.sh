7717c8407affd3e22100b6ba72f8a379e9d52fd5
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2010-03-03 01:45:08 +0000

    linux : slight adjustment, simplification as per Erich
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@6179 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/script/run.sh
+++ genj/script/run.sh
@@ -1,25 +1,21 @@
 #!/bin/sh
 
-# GenJ has to be run from inside it's installation directory
-# so change into directory where this script was started
+# cd to application directory
 cd `dirname $0`
 
 # check the script for being a symbolik link we can follow
 SCRIPT=`basename $0`
 while [ -h "$SCRIPT" ]; do
  SCRIPT=`ls -l $SCRIPT | grep -o '[-_/.[:alnum:]]*$'`
- echo "*** INFO: Following symlink $SCRIPT"
  cd `dirname $SCRIPT`
  SCRIPT=`basename $SCRIPT`
 done
 
-# final check if the GenJ main archive is right here
+# find main archive
 if [ ! -f "./run.jar"  ]; then
  echo "*** ERROR: Missing GenJ resource(s) in "`pwd`
  exit 1
 fi
-
-echo "*** INFO: Running GenJ from"`pwd`
 
 # find java
 JAVA=$JAVA_HOME/bin/java
@@ -31,9 +27,5 @@ if [ ! -x "$JAVA" ]; then
  fi
 fi
 
-# run it (we start the virtual machine with initially 32 MB and allocate a max of 512 MB)
-CMD="$JAVA -Xmx512m -Xms32m -Djava.net.preferIPv4Stack=true -jar run.jar $1 $2 $3 $4 $5 $6 $7 $8 $9"
-
-echo "*** INFO: Executing '$CMD'"
-
-$CMD
+# run it 
+$JAVA -Xmx512m -Xms32m -Djava.net.preferIPv4Stack=true -jar run.jar $@
