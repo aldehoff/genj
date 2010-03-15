20c38bb65806aff741b6620bebacc381372cc62c
francoismz <francoismz@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2010-03-15 23:43:40 +0000

    Reports g-v: updated/created translations fr & de (FrancoisMz) + standardized translation presentation a la Paul
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@6244 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/src/report/ReportRecompile.java
+++ genj/src/report/ReportRecompile.java
@@ -30,23 +30,17 @@ import java.util.List;
  *  <p>For the compilation to succeed GenJ needs to be run inside a JDK's Java Virtual Machine, not in 
  *  a JRE's Java Virtual Machine.</p>
  *  
- * info.de = Dieser Report kompiliert alle Reports im GenJ Reportverzeichnis (./report). Nach Neustart von 
- *  GenJ oder einem Klick auf den Schalter 'Neu Einladen' in ReportView können kompilierte Reports neu eingeladen werden.\n\n
- *  Die Kompilierung funktioniert nur, wenn GenJ in einer JDK Java Virtual Machine, nicht in einer JRE JVM, läuft.
- * info.fr =  <h1><center>Outil de Compilation des Rapports</center></h1><br>
- *   <div style="margin-left: 5px; text-align:justify; 
- *   margin-right: 10px; font-style: normal; color: #000000">
- *   Ce rapport vous sert à recompiler tous les rapports qui se trouvent 
- *   dans le répertoire des rapports de GenJ (./report).<br>
- *   Aprés un redémarrage de GenJ ou aprés avoir cliqué sur le bouton de 
- *   rechargement des Scripts, tous les rapports nouvellement compilés 
- *   pourront être utilisés par GenJ.<br>
- *   <br>
- *   Pour que cela fonctionne, il vous faut cependant que GenJ tourne 
- *   à l'intérieur d'une Machine Virtuelle Java JDK, et non une Machine 
- *   Virtuelle Java JRE. Pour faire simple, il faut que le programme 
- *   Java que vous avez téléchargé sur le site de Sun commence par "jdk", 
- *   et non par "jre". 
+ * info.de = <h1><center>Reports rekompilieren</center></h1>
+ *  <p>Dieser Report kompiliert alle Reports im GenJ Reportverzeichnis (./report). Nach Neustart von 
+ *  GenJ oder einem Klick auf den Schalter 'Neu Einladen' in ReportView können kompilierte Reports neu eingeladen werden.</p>
+ *  <p>Die Kompilierung funktioniert nur, wenn GenJ in einer JDK Java Virtual Machine, nicht in einer JRE JVM, läuft.</p>
+ *
+ * info.fr =  <h1><center>Outil de Compilation des Rapports</center></h1>
+ *   <p>Ce rapport vous sert à recompiler tous les rapports qui se trouvent dans le répertoire des rapports de GenJ (./report).</p>
+ *   <p>Après un redémarrage de GenJ ou après avoir cliqué sur le bouton de rechargement des rapports, tous les rapports nouvellement compilés 
+ *   pourront être utilisés par GenJ.</p>
+ *   <p>Pour que cela fonctionne, il faut cependant que GenJ tourne à l'intérieur d'une Machine Virtuelle Java JDK, et non une Machine Virtuelle Java JRE.
+ *   Pour faire simple, il faut que le programme Java que vous avez téléchargé sur le site de Sun commence par "jdk", et non par "jre". </p>
  *  
  * nosources = No sources in {0}
  * nosources.de = Keine Quelldateien in {0}
@@ -54,23 +48,19 @@ import java.util.List;
  * 
  * javac.success = {0} Sources (*.java) compiled into {1} - to activate press 'Reload report classes'
  * javac.success.de = {0} Quelldateien (*.java) kompiliert nach {1} - zum Aktivieren 'Neu Einladen' klicken 
- * javac.success.fr = \n\n{0} Fichiers Sources (*.java) ont été compilées 
+ * javac.success.fr = \n\n{0} Fichiers Sources (*.java) ont été compilés 
  *  dans le répertoire :\n     {1}\n\n
  *  ATTENTION : Pour que cette compilation soit prise en considération,\n
- *              Veuillez cliquer sur le bouton :\n
- *              'Recharger les scripts de lancement des Rapports' \n
- *              (en bas de la fenêtre, le 4ème à partir de la gauche).
+ *              Veuillez cliquer sur le bouton de rechargement des rapports (2 flèches vertes imbriquées).
  * 
  * javac.error = Compilation failed - check compiler output above
  * javac.error.de = Kompilierung fehlgeschlagen - bitte Fehlermeldung(en) oben konsultieren
- * javac.error.fr = La compilation a échouée - Vérifiez les messages de 
- *  sortie du compilateur ci-dessus.
+ * javac.error.fr = La compilation a échouée - Vérifiez les messages de sortie du compilateur ci-dessus.
  * 
  * javac.jre = Reports can't be recompiled - make sure GenJ is run inside a JDK's Java Virtual Machine (java.home={0}) 
  * javac.jre.de = Reports können nicht kompiliert werden - GenJ muß in einer JDK Java Virtual Machine ablaufen (java.home={0}) 
- * javac.jre.fr = \nLes Rapports n'ont pas pu etre compilés.\n
- *  Assurez-vous que GenJ est lancé par l'intermédiaire de la machine 
- *  virtuelle Java JDK (JDK et non JRE).\n\n     Pour information : (java.home={0}) 
+ * javac.jre.fr = \nLes Rapports n'ont pas pu être compilés.\n
+ *  Assurez-vous que GenJ est lancé par l'intermédiaire de la machine virtuelle Java JDK (JDK et non JRE).\n\n     Pour information : (java.home={0}) 
  * 
  */
 public class ReportRecompile extends Report {
