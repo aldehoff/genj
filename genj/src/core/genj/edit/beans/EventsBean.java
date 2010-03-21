55ec2fb992908b7d4855ca5c9273b92dd59134df
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2010-03-21 00:35:03 +0000

    ui: play with events/sources/notes as suggested by Paul
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@6272 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/src/core/genj/edit/beans/EventsBean.java
+++ genj/src/core/genj/edit/beans/EventsBean.java
@@ -22,12 +22,16 @@ package genj.edit.beans;
 import genj.edit.ChoosePropertyBean;
 import genj.edit.Images;
 import genj.gedcom.Gedcom;
+import genj.gedcom.Grammar;
 import genj.gedcom.MetaProperty;
 import genj.gedcom.Property;
 import genj.gedcom.PropertyComparator;
 import genj.gedcom.PropertyEvent;
+import genj.gedcom.PropertyNote;
+import genj.gedcom.TagPath;
 import genj.util.swing.Action2;
 import genj.util.swing.DialogHelper;
+import genj.util.swing.ImageIcon;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
@@ -37,28 +41,59 @@ import java.util.Collections;
 import java.util.List;
 
 import javax.swing.Action;
+import javax.swing.Icon;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableColumnModel;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
 
 /**
  * A complex bean displaying events of an individual or family
  */
 public class EventsBean extends PropertyBean {
+  
+  private final static Col[] COLS = {
+      new EventCol(),
+      new DetailCol(),
+      new ValueCol("DATE"),
+      new ValueCol("PLAC"),
+      new NoteCol(),
+      new SourceCol(),
+      new EditCol(),
+      new DelCol()
+  };
+  
+  private final static ImageIcon 
+  SOURCE = Grammar.V551.getMeta(new TagPath("SOUR")).getImage(),
+  NOTE = Grammar.V551.getMeta(new TagPath("NOTE")).getImage();
 
   private JTable table;
+  private Object commit;
   
   public EventsBean() {
     
     // prepare a simple table
-    table = new JTable(new Events(null));
+    table = new JTable(new Events(null), columns());
     table.setPreferredScrollableViewportSize(new Dimension(32,32));
     table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
     
     setLayout(new BorderLayout());
     add(BorderLayout.CENTER, new JScrollPane(table));
 
+  }
+  
+  private TableColumnModel columns() {
+    DefaultTableColumnModel result = new DefaultTableColumnModel();
+    for (int i=0; i<COLS.length; i++) {
+      TableColumn c = new TableColumn(i);
+      c.setMaxWidth(COLS[i].max);
+      c.setHeaderValue(COLS[i].getName());
+      result.addColumn(c);
+    }
+    return result;
   }
   
   @Override
@@ -68,6 +103,20 @@ public class EventsBean extends PropertyBean {
   
   @Override
   protected void commitImpl(Property property) {
+    
+    if (commit instanceof String) {
+      Property event = property.addProperty((String)commit, "");
+      commit = null;
+      ((Events)table.getModel()).add(event);
+    }
+    
+    if (commit instanceof Property) {
+      Property event = (Property)commit;
+      if (event.getParent()!=null)
+        event.getParent().delProperty(event);
+      ((Events)table.getModel()).del(event);
+    }
+    
   }
   
   private boolean isEvent(MetaProperty meta) {
@@ -89,19 +138,13 @@ public class EventsBean extends PropertyBean {
 
   @Override
   protected void setPropertyImpl(Property prop) {
+    
+    commit = null;
+    
     table.setModel(new Events(prop));
   }
   
   private class Events extends AbstractTableModel {
-    
-    private final String[] COLS = {
-      Gedcom.getName("EVEN"), 
-      RESOURCES.getString("even.detail"),
-      Gedcom.getName("DATE"),
-      Gedcom.getName("PLAC"),
-      Gedcom.getName("NOTE"),
-      Gedcom.getName("SOUR", true)
-    };
     
     private List<Property> rows = new ArrayList<Property>();
     
@@ -117,9 +160,30 @@ public class EventsBean extends PropertyBean {
         rows.add(child);
       }
       
-      Collections.sort(rows, new PropertyComparator(".:BIRT"));
+      Collections.sort(rows, new PropertyComparator(".:DATE"));
       
       // done
+    }
+    
+    void add(Property event) {
+      rows.add(0, event);
+      fireTableRowsInserted(0, 0);
+    }
+    
+    void del(Property event) {
+      for (int i=0;i<rows.size();i++) {
+        if (rows.get(i)==event) {
+          rows.remove(i);
+          fireTableRowsDeleted(i, i);
+          return;
+        }
+      }
+      throw new IllegalArgumentException("no such "+event);
+    }
+    
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+      return COLS[columnIndex].getType();
     }
     
     @Override
@@ -134,30 +198,12 @@ public class EventsBean extends PropertyBean {
     
     @Override
     public String getColumnName(int column) {
-      return COLS[column];
+      return COLS[column].getName();
     }
     
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-      Property event = rows.get(rowIndex);
-      switch (columnIndex) {
-        case 0:
-          return event.getPropertyName();
-        case 1:
-          String val = event.getDisplayValue();
-          if (val.length()>0)
-            return val;
-          return event.getPropertyValue("TYPE");
-        case 2:
-          return event.getPropertyDisplayValue("DATE");
-        case 3:
-          return event.getPropertyDisplayValue("PLAC");
-        case 4:
-          return "yes";
-        case 5:
-          return "no";
-      }
-      throw new IllegalArgumentException("no such column "+columnIndex);
+      return COLS[columnIndex].getValue(rows.get(rowIndex));
     }
   }
   
@@ -188,7 +234,7 @@ public class EventsBean extends PropertyBean {
           choose, Action2.okCancel(), EventsBean.this))
         return;
       
-      String add = choose.getSelectedTags()[0];
+      commit = choose.getSelectedTags()[0];
       
       EventsBean.this.changeSupport.fireChangeEvent(new CommitRequired(EventsBean.this));
             
@@ -236,5 +282,119 @@ public class EventsBean extends PropertyBean {
 //      
 //    }
 //  } //Del
+
+  private static abstract class Col {
+    protected Class<?> type;
+    protected int max = Integer.MAX_VALUE;
+    Col() {
+      type = String.class;
+    }
+    Class<?> getType() {
+      return type;
+    }
+    String getName() {
+      return "";
+    }
+    abstract Object getValue(Property event);
+    int getMax() {
+      return max;
+    }
+  }
   
+  private static class EventCol extends Col {
+    @Override
+    String getName() {
+      return Gedcom.getName("EVEN");
+    }
+    @Override
+    Object getValue(Property prop) {
+      return prop.getPropertyName();
+    }
+  }
+  
+  private static class ValueCol extends Col {
+    private String tag, name;
+    ValueCol(String tag) {
+      this(tag, Gedcom.getName(tag));
+    }
+    ValueCol(String tag, String name) {
+      this.tag = tag;
+      this.name = name;
+    }
+    @Override
+    String getName() {
+      return name;
+    }
+    @Override
+    Object getValue(Property event) {
+      return event.getPropertyDisplayValue(tag);
+    }
+    
+  }
+  
+  private static class DetailCol extends Col {
+    @Override
+    String getName() {
+      return RESOURCES.getString("even.detail");
+    }
+    @Override
+    Object getValue(Property event) {
+      String val = event.getDisplayValue();
+      if (val.length()>0)
+        return val;
+      return event.getPropertyValue("TYPE");
+    }
+  }
+  
+  private static class NoteCol extends Col {
+    NoteCol() {
+      type = Icon.class;
+      max = Gedcom.getImage().getIconWidth();
+    }
+    @Override
+    Object getValue(Property event) {
+      for (Property note : event.getProperties("NOTE")) {
+        if (note instanceof PropertyNote)
+          return NOTE;
+        if (note.getValue().length()>0)
+          return NOTE;
+      }
+      return NOTE.getGrayedOut();
+    }
+  }
+ 
+  private static class SourceCol extends Col {
+    SourceCol() {
+      type = Icon.class;
+      max = Gedcom.getImage().getIconWidth();
+    }
+    @Override
+    Object getValue(Property event) {
+      for (Property source : event.getProperties("SOUR")) {
+        return SOURCE;
+      }
+      return SOURCE.getGrayedOut();
+    }
+  }
+  private static class EditCol extends Col {
+    EditCol() {
+      type = Icon.class;
+      max = Gedcom.getImage().getIconWidth();
+    }
+    @Override
+    Object getValue(Property event) {
+      return Images.imgAdvanced;
+    }
+  }
+  
+  private static class DelCol extends Col {
+    DelCol() {
+      type = Icon.class;
+      max = Gedcom.getImage().getIconWidth();
+    }
+    @Override
+    Object getValue(Property event) {
+      return Images.imgDel;
+    }
+  }
 }
