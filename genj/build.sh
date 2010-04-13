bd17794506140a6964f1bee4cded6cac05f1a7fe
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2010-04-13 20:27:00 +0000

    make build work without JAVA_HOME if java is on path
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@6425 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/build.sh
+++ genj/build.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 #
 
+if [ ! -d "$JAVA_HOME" ]; then
+  JAVA=`which java`
+  BIN=`dirname $JAVA`
+  JAVA_HOME=${BIN%/*}
+fi 
+
 $JAVA_HOME/bin/java -cp $JAVA_HOME/lib/tools.jar:./contrib/ant/ant.jar org.apache.tools.ant.Main $1 $2 $3
 
