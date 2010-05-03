684a77a9d3140fde42b9c142f56d7451893cc440
francoismz <francoismz@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2010-05-03 20:53:34 +0000

    pedigree: management of long names / yEd: German presentation
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@6542 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/src/report/ReportPedigree.java
+++ genj/src/report/ReportPedigree.java
@@ -14,7 +14,7 @@ version   = 1.0 - 23 Jan 2010
 */
 public class ReportPedigree extends Report {
 	 
-		
+
 	//translate strings for output  
 	private String textTitle = translate("title");
 	private String textSelect = translate("select");
@@ -60,39 +60,42 @@ public class ReportPedigree extends Report {
 	  Indi iDiagIndi[] = new Indi[31];
 	  int iDiag[][] = {
 			  {-1,-1,-1,-2,15},
-			  {-1,-1,-2,7,-1},
+			  {-1,-1,-2,7,-4},
 			  {-1,-1,-3,-2,16},
-			  {-1,-2,3,-1,-1},
+			  {-1,-2,3,-4,-4},
 			  {-1,-3,-3,-2,17},
-			  {-1,-3,-2,8,-1},
+			  {-1,-3,-2,8,-4},
 			  {-1,-3,-1,-2,18},
-			  {-2,1,-1,-1,-1},
+			  {-2,1,-4,-4,-4},
 			  {-3,-3,-1,-2,19},
-			  {-3,-3,-2,9,-1},
+			  {-3,-3,-2,9,-4},
 			  {-3,-3,-3,-2,20},
-			  {-3,-2,4,-1,-1},
+			  {-3,-2,4,-4,-4},
 			  {-3,-1,-3,-2,21},
-			  {-3,-1,-2,10,-1},
+			  {-3,-1,-2,10,-4},
 			  {-3,-1,-1,-2,22},
-			  {0,-1,-1,-1,-1},
+			  {0,-4,-4,-4,-4},
 			  {-3,-1,-1,-2,23},
-			  {-3,-1,-2,11,-1},
+			  {-3,-1,-2,11,-4},
 			  {-3,-1,-3,-2,24},
-			  {-3,-2,5,-1,-1},
+			  {-3,-2,5,-4,-4},
 			  {-3,-3,-3,-2,25},
-			  {-3,-3,-2,12,-1},
+			  {-3,-3,-2,12,-4},
 			  {-3,-3,-1,-2,26},
-			  {-2,2,-1,-1,-1},
+			  {-2,2,-4,-4,-4},
 			  {-1,-3,-1,-2,27},
-			  {-1,-3,-2,13,-1},
+			  {-1,-3,-2,13,-4},
 			  {-1,-3,-3,-2,28},
-			  {-1,-2,6,-1,-1},
+			  {-1,-2,6,-4,-4},
 			  {-1,-1,-3,-2,29},
-			  {-1,-1,-2,14,-1},
+			  {-1,-1,-2,14,-4},
 			  {-1,-1,-1,-2,30}	  		  
 	  };
 	  String strRow;
-	   
+	  
+	 //huge text field length (for names+dates) to be sure to accomodate all sorts of name lengths - will be trimmed eventually
+	 int iTextFieldLength = 80;
+	 
 	  //initialise parse list with root person
 	  parseList.add(indi);
 	
@@ -163,12 +166,12 @@ public class ReportPedigree extends Report {
 			  //get item to print
 			  iTemp = iDiag[iRow][iCol];
 			  if(iTemp<0){
-				  if(iTemp==-1)strRow=strRow + align(" ",24, 3);				 
+				  if(iTemp==-1)strRow=strRow + align(" ",24, 0);
 				  if(iTemp==-2)strRow=strRow + align("|-------------",24, 2);
 				  if(iTemp==-3)strRow=strRow + align("|             ",24, 2);
 			  }
 			  	else {
-			  		if(iDiagIndi[iTemp].getName()=="") strRow=strRow + align("("+textUnknown+ ")",32,3);
+			  		if(iDiagIndi[iTemp].getName()=="") strRow=strRow + align("("+textUnknown+ ")",iTextFieldLength,0).trim();
 			  			else {
 			  				strDates="";
 			  				iDOBYear = getYear(iDiagIndi[iTemp].getBirthDate());
@@ -177,7 +180,7 @@ public class ReportPedigree extends Report {
 			  			    iDODYear = getYear(iDiagIndi[iTemp].getDeathDate());
 			  			    if(iDODYear==-1) strDates = strDates + ")";
 		  						else strDates = strDates + iDODYear + ")";
-			  				strRow=strRow + align(iDiagIndi[iTemp].getName()+strDates, 32, 3);
+			  				strRow=strRow + align(iDiagIndi[iTemp].getName()+strDates, iTextFieldLength, 0).trim();
 
 			  			}
 			  	}
