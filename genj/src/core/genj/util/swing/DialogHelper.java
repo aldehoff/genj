f05dfdf7f21d9c31b333a6f6db2e52cfcce01f28
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2010-06-12 20:46:06 +0000

    ui: expose dialog object for on-dialog actions like cancel
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@6570 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/src/core/genj/util/swing/DialogHelper.java
+++ genj/src/core/genj/util/swing/DialogHelper.java
@@ -24,9 +24,7 @@ import genj.util.Registry;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Container;
-import java.awt.Dialog;
 import java.awt.Dimension;
-import java.awt.Frame;
 import java.awt.HeadlessException;
 import java.awt.Rectangle;
 import java.awt.Toolkit;
@@ -35,7 +33,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
-import java.util.ArrayList;
 import java.util.EventObject;
 import java.util.List;
 import java.util.StringTokenizer;
@@ -68,8 +65,6 @@ public class DialogHelper {
 
   /** screen we're dealing with */
   private final static Rectangle screen = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
-  
-  private static List<JDialog> dialogs = new ArrayList<JDialog>();
   
   /** message types*/
   public static final int  
@@ -190,15 +185,7 @@ public class DialogHelper {
   }
 
   public static int openDialog(String title, int messageType,  JComponent content, Action[] actions, Object source) {
-    // check options - default to OK
-    if (actions==null) 
-      actions = Action2.okOnly();
-    // do it
-    Object rc = openDialogImpl(title, messageType, content, actions, source);
-    // analyze - check which action was responsible for close
-    for (int a=0; a<actions.length; a++) 
-      if (rc==actions[a]) return a;
-    return -1;
+    return new Dialog(title, messageType, content, actions, source).show();
   }
 
   public static Window getWindow(EventObject event) {
@@ -213,74 +200,90 @@ public class DialogHelper {
     });
   }
   
-  /**
-   * Dialog implementation
-   */
-  private static Object openDialogImpl(String title, int messageType, final JComponent content, Action[] actions, Object source) {
+  public static class Dialog {
     
-    // find window for source
-    Component parent = null;
-    if (source instanceof Component)
-      parent = (Component)source;
-    else if (source instanceof EventObject && ((EventObject)source).getSource() instanceof Component)
-      parent = visitOwners( (Component)((EventObject)source).getSource(), new ComponentVisitor() {
-        public Component visit(Component parent, Component child) {
-          return parent ==null ? child : null;
+    private String title;
+    private int messageType;
+    private final JComponent content;
+    private Action[] actions;
+    private Component parent;
+    private JDialog dlg;
+
+    public Dialog(String title, int messageType, final JComponent content, Action[] actions, Object source) {
+      
+      this.title = title;
+      this.messageType = messageType;
+      this.content = content;
+      this.actions = actions!=null ? actions : Action2.okOnly();
+      
+      // find window for source
+      parent = null;
+      if (source instanceof Component)
+        parent = (Component)source;
+      else if (source instanceof EventObject && ((EventObject)source).getSource() instanceof Component)
+        parent = visitOwners( (Component)((EventObject)source).getSource(), new ComponentVisitor() {
+          public Component visit(Component parent, Component child) {
+            return parent ==null ? child : null;
+          }
+        });
+      
+      // patch opaqueness of content
+      patchOpaque(content, true);
+
+      // done for now
+    }
+    
+    public int show() {
+      
+      // create an option pane
+      final JOptionPane optionPane = new Content(messageType, content, actions);
+      
+      // create the dialog and content
+      dlg = optionPane.createDialog(parent, title);
+      dlg.setResizable(true);
+      dlg.setModal(true);
+      dlg.pack();
+      dlg.setMinimumSize(content.getMinimumSize());
+
+      // restore bounds
+      StackTraceElement caller = getCaller();
+      final Registry registry = Registry.get(caller.getClassName());
+      final String key = caller.getMethodName() + (caller.getLineNumber()>0?caller.getLineNumber():"") + ".dialog";
+      Dimension bounds = registry.get(key, (Dimension)null);
+      if (bounds!=null) {
+        bounds.width = Math.max(bounds.width, dlg.getWidth());
+        bounds.height = Math.max(bounds.height, dlg.getHeight());
+        dlg.setBounds(new Rectangle(bounds).intersection(screen));
+      }
+      dlg.setLocationRelativeTo(parent);
+
+      // hook up to the dialog being hidden by the optionpane - that's what is being called after the user selected a button (setValue())
+      dlg.addComponentListener(new ComponentAdapter() {
+        public void componentHidden(ComponentEvent e) {
+          registry.put(key, dlg.getSize());
+          dlg.dispose();
         }
       });
-    
-    // patch opaqueness of content
-    patchOpaque(content, true);
-
-    // create an option pane
-    final JOptionPane optionPane = new Content(messageType, content, actions);
-    
-    // create the dialog and content
-    final JDialog dlg = optionPane.createDialog(parent, title);
-    dlg.setResizable(true);
-    dlg.setModal(true);
-    dlg.pack();
-    dlg.setMinimumSize(content.getMinimumSize());
-
-    // restore bounds
-    StackTraceElement caller = getCaller();
-    final Registry registry = Registry.get(caller.getClassName());
-    final String key = caller.getMethodName() + (caller.getLineNumber()>0?caller.getLineNumber():"") + ".dialog";
-    Dimension bounds = registry.get(key, (Dimension)null);
-    if (bounds!=null) {
-      bounds.width = Math.max(bounds.width, dlg.getWidth());
-      bounds.height = Math.max(bounds.height, dlg.getHeight());
-      dlg.setBounds(new Rectangle(bounds).intersection(screen));
-    }
-    dlg.setLocationRelativeTo(parent);
-
-    // hook up to the dialog being hidden by the optionpane - that's what is being called after the user selected a button (setValue())
-    dlg.addComponentListener(new ComponentAdapter() {
-      public void componentHidden(ComponentEvent e) {
-        registry.put(key, dlg.getSize());
-        dlg.dispose();
+      
+      // show it
+      try {
+        dlg.setVisible(true);
+      } finally {
+        dlg = null;
       }
-    });
-    
-    // show it
-    try {
-      dialogs.add(dlg);
-      dlg.setVisible(true);
-    } finally {
-      dialogs.remove(dlg);
+      
+      // analyze - check which action was responsible for close
+      Object rc = optionPane.getValue();
+      for (int a=0; a<actions.length; a++) 
+        if (rc==actions[a]) return a;
+      return -1;
     }
     
-    // return result
-    return optionPane.getValue();
-  }
-
-  /**
-   * Cancels the last open dialog
-   */
-  public static void cancelDialog() {
-    if (dialogs.isEmpty())
-      throw new IllegalArgumentException("no dialog to close");
-    dialogs.get(dialogs.size()-1).dispose();
+    public void cancel() {
+      if (dlg==null)
+        throw new IllegalStateException("not showing");
+      dlg.dispose();
+    }
   }
   
   private static StackTraceElement getCaller() {
@@ -292,15 +295,6 @@ public class DialogHelper {
     return new StackTraceElement("Class", "method", "file", 0);
   }
 
-  /**
-   * Get the window for given owner component
-   */  
-  private Window getWindowForComponent(Component c) {
-    if (c instanceof Frame || c instanceof Dialog || c==null)
-      return (Window)c;
-    return getWindowForComponent(c.getParent());
-  }
-  
   /**
    * A patched up JOptionPane
    */
