c538895a86574f5efabe3781f9b5a8106501d3dd
francoismz <francoismz@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2011-09-14 05:31:39 +0000

    ReportMissing: added source verification + CSV-like export
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@6752 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/src/report/ReportMissingInformation.java
+++ genj/src/report/ReportMissingInformation.java
@@ -2,8 +2,10 @@ import genj.gedcom.Entity;
 import genj.gedcom.Gedcom;
 import genj.gedcom.Indi;
 import genj.gedcom.PropertyDate;
+import genj.gedcom.PropertyEvent;
 import genj.gedcom.PropertyPlace;
 import genj.gedcom.PropertySex;
+import genj.gedcom.PropertySource;
 import genj.gedcom.TagPath;
 import genj.gedcom.time.PointInTime;
 import genj.report.Report;
@@ -17,35 +19,50 @@ import genj.report.Report;
 public class ReportMissingInformation extends Report {
 	 
 	// check all relevant tags by default	
+	public boolean checkIndividualSource = true;
 	public boolean checkBirthDate = true;
 	public boolean checkBirthPlace = true;
+	public boolean checkBirthSource = true;
 	public boolean checkBaptismDate = true;
 	public boolean checkBaptismPlace = true;
+	public boolean checkBaptismSource = true;
 	public boolean checkDeathDate = true;
 	public boolean checkDeathPlace = true;
+	public boolean checkDeathSource = true;
 	public boolean checkSex = true;
 	public boolean checkGiven = true;
 	public boolean checkSurname = true;
+	public boolean checkOccupation = true;
+	public boolean checkOccupationSource = true;
+	public boolean formatForScreenDisplay = true;
   
 	//translate strings for output  
 	private String textTitle = translate("title");
 	private String textSubject = translate("subject"); 
+	private String textIndividual = translate("individual");
 	private String textBirth = translate("birth");
 	private String textBaptism = translate("baptism");
 	private String textDeath = translate("death");
 	private String textDate = translate("date");
 	private String textPlace = translate("place");
+	private String textSource = translate("source");
 	private String textSex = translate("sex");
 	private String textGiven = translate("given");
 	private String textSurname = translate("surname");
+	private String textOccupation = translate("occupation");
 	private String textKey = translate("key");
 
 	
 	//column widths etc
+	private int colId = 6;
 	private int colName = 30;
 	private int colData = 6;
-	private int numDataCols = 9;
-  
+	private int numDataCols = 0;
+
+	private String formatCol(String tempString, int colWidth, int colAlign) {
+		return ( formatForScreenDisplay ? align(tempString, colWidth, colAlign) : "\t" + tempString);
+	}
+	
   public void start(Indi indi) {
 	  
    	 
@@ -62,41 +79,62 @@ public class ReportMissingInformation extends Report {
 	//vars
 	PropertyDate tempDate;
 	PropertyPlace tempPlace;
+	PropertySource tempSource;
 	String strDataRow;
-	String strNameID;
-	Boolean flagOk1, flagOk2;
+	Boolean flagOk1, flagOk2, flagOk3;
 
 	
     //clear any previous data and align
-	strNameID = indi.getName() + " " + indi.getId();
-    strDataRow = align(strNameID, colName, 3); 
+    strDataRow = indi.getId() + (formatForScreenDisplay ? "\t" : "") + formatCol(indi.getName(), colName, 3); 
 
     //NOTE: the order of the following tests corresponds with the display column order
     
+ 	//check individual source if required
+	if(checkIndividualSource) {		
+		// verify existence and validity of source tag
+		tempSource = (PropertySource)indi.getProperty(new TagPath("INDI:SOUR"));
+		if((tempSource == null) || (!tempSource.isValid())) {
+			strDataRow = strDataRow + formatCol("X",colData,1);
+		} else { 
+			strDataRow = strDataRow + formatCol("ok",colData,1);	
+		}
+	}
+
  	//check birth date if required
 	if(checkBirthDate) {		
 		//read date of birth for validity checking
 		tempDate = indi.getBirthDate();
 		if((tempDate == null) || (!tempDate.isValid())) {
-			strDataRow = strDataRow + align("X",colData,1);
+			strDataRow = strDataRow + formatCol("X",colData,1);
 		} else { 
-			strDataRow = strDataRow + align("ok",colData,1);	
+			strDataRow = strDataRow + formatCol("ok",colData,1);	
 		}
-	} else {
-		strDataRow = strDataRow + align("-",colData,1);	// not checked
 	}
 	
 	//check place of birth if required
 	if(checkBirthPlace) {
 		tempPlace = (PropertyPlace)indi.getProperty(new TagPath("INDI:BIRT:PLAC"));
 		if((tempPlace == null)){
-			strDataRow = strDataRow + align("X",colData,1);
-		}else {
-			strDataRow = strDataRow + align("ok",colData,1);	
+			strDataRow = strDataRow + formatCol("X",colData,1);
+		} else {
+			strDataRow = strDataRow + formatCol("ok",colData,1);	
 		}
-		
-	} else {
-		strDataRow = strDataRow + align("-",colData,1);	// not checked
+	}
+ 
+	//check birth source if required
+	if(checkBirthSource) {		
+		// verify existence of birth event
+		if((PropertyEvent)indi.getProperty(new TagPath("INDI:BIRT")) != null) {		
+			// verify existence and validity of source tag
+			tempSource = (PropertySource)indi.getProperty(new TagPath("INDI:BIRT:SOUR"));
+			if((tempSource == null) || (!tempSource.isValid())) {
+				strDataRow = strDataRow + formatCol("X",colData,1);
+			} else { 
+				strDataRow = strDataRow + formatCol("ok",colData,1);	
+			}
+		} else {
+			strDataRow = strDataRow + formatCol("-",colData,1);	
+		}
 	}
 	
 	//check baptism and christening date if required
@@ -118,19 +156,13 @@ public class ReportMissingInformation extends Report {
 	
 		//if date found on either tag, flag is true
 		if(flagOk1 || flagOk2) {
-			strDataRow = strDataRow + align("ok",colData,1);
+			strDataRow = strDataRow + formatCol("ok",colData,1);
 		} else {
-		strDataRow = strDataRow + align("X",colData,1);
-			
+			strDataRow = strDataRow + formatCol("X",colData,1);
 		}
-		
-	}
-	else {
-		strDataRow = strDataRow + align("-",colData,1);
 	}
 	
-	
-	//baptism place
+	//check baptism place if required
 	if(checkBaptismPlace) {
 		
 		flagOk1 = true;
@@ -138,7 +170,6 @@ public class ReportMissingInformation extends Report {
 		
 		//check <bapt> 
 		tempPlace = (PropertyPlace)indi.getProperty(new TagPath("INDI:BAPM:PLAC"));
-		//tempPlace2 = (PropertyPlace)indi.getProperty(new TagPath("INDI:CHR:PLAC"));
 		if((tempPlace == null) || (tempPlace.getValue() == "")) {
 			flagOk1 = false;
 		}
@@ -150,55 +181,90 @@ public class ReportMissingInformation extends Report {
 		}		
 			
 		if(flagOk1 || flagOk2) {	
-			strDataRow = strDataRow + align("ok",colData,1);
+			strDataRow = strDataRow + formatCol("ok",colData,1);
 		} else { 
-			strDataRow = strDataRow + align("X",colData,1);	
+			strDataRow = strDataRow + formatCol("X",colData,1);	
 		}		
 	}
-	else {
-		strDataRow = strDataRow + align("-",colData,1);
-	}
-	
-	
+
+	//check baptism source if required
+	if(checkBaptismSource) {		
+		flagOk1 = true;
+		flagOk2 = true;
+		flagOk3 = true;
+		// verify existence of baptism event
+		if((PropertyEvent)indi.getProperty(new TagPath("INDI:BAPM")) == null) {		
+			flagOk1 = false;
+		}
+		if((PropertyEvent)indi.getProperty(new TagPath("INDI:CHR")) == null) {		
+			flagOk2 = false;
+		}
+
+		if((!flagOk1) && (!flagOk2)) {
+			strDataRow = strDataRow + formatCol("-",colData,1);
+		} else {
+			if(flagOk1) {
+				// verify existence and validity of source tag
+				tempSource = (PropertySource)indi.getProperty(new TagPath("INDI:BAPM:SOUR"));
+				if((tempSource == null) || (!tempSource.isValid())) flagOk3 = false;
+			}
+			if(flagOk2) {
+				// verify existence and validity of source tag
+				tempSource = (PropertySource)indi.getProperty(new TagPath("INDI:CHR:SOUR"));
+				if((tempSource == null) || (!tempSource.isValid())) flagOk3 = false;
+			}
+			if(!flagOk3) {
+				strDataRow = strDataRow + formatCol("X",colData,1);
+			} else { 
+				strDataRow = strDataRow + formatCol("ok",colData,1);	
+			}			
+		}
+	}	
 	
 	
 	//check death date if required
 	if(checkDeathDate) {
-		
-		//reset flags
-		
 		tempDate = indi.getDeathDate();
 		if((indi.getDeathDate() == null) || (!tempDate.isValid())) {
-			strDataRow = strDataRow + align("X",colData,1);
+			strDataRow = strDataRow + formatCol("X",colData,1);
 		} else {
-		strDataRow = strDataRow + align("ok",colData,1);
+		strDataRow = strDataRow + formatCol("ok",colData,1);
 		}
-	} else {
-		strDataRow = strDataRow + align("-",colData,1);	
 	}
 	
 	//check place of death if required
 	if(checkDeathPlace) {
 		tempPlace = (PropertyPlace)indi.getProperty(new TagPath("INDI:DEAT:PLAC"));
 		if((tempPlace == null)){
-			strDataRow = strDataRow + align("X",colData,1);
+			strDataRow = strDataRow + formatCol("X",colData,1);
 		}else {
-			strDataRow = strDataRow + align("ok",colData,1);	
+			strDataRow = strDataRow + formatCol("ok",colData,1);	
 		}
-		
-	} else {
-		strDataRow = strDataRow + align("-",colData,1);	// not checked
 	}	
 
+	//check death source if required
+	if(checkDeathSource) {		
+		// verify existence of death event
+		if((PropertyEvent)indi.getProperty(new TagPath("INDI:DEAT")) != null) {		
+			// verify existence and validity of source tag
+			tempSource = (PropertySource)indi.getProperty(new TagPath("INDI:DEAT:SOUR"));
+			if((tempSource == null) || (!tempSource.isValid())) {
+				strDataRow = strDataRow + formatCol("X",colData,1);
+			} else { 
+				strDataRow = strDataRow + formatCol("ok",colData,1);	
+			}
+		} else {
+			strDataRow = strDataRow + formatCol("-",colData,1);	
+		}
+	}
+	
 	//check gender if required
 	if(checkSex) {
 		if((indi.getSex() != PropertySex.MALE) && (indi.getSex() != PropertySex.FEMALE)) { 
-			strDataRow = strDataRow + align("X",colData,1);
+			strDataRow = strDataRow + formatCol("X",colData,1);
 		} else {	
-		strDataRow = strDataRow + align("ok",colData,1);
+			strDataRow = strDataRow + formatCol("ok",colData,1);
 		}
-	} else {
-		strDataRow = strDataRow + align("-",colData,1);	
 	}
 	
 
@@ -206,12 +272,10 @@ public class ReportMissingInformation extends Report {
 	// uses extraction from <name> rather than checking <GIVN>
 	if(checkGiven) {
 		if(indi.getFirstName() == "") {
-			strDataRow = strDataRow + align("X",colData,1);
-	} else {
-		strDataRow = strDataRow + align("ok",colData,1);
-	}
-	} else {
-		strDataRow = strDataRow + align("-",colData,1);	
+			strDataRow = strDataRow + formatCol("X",colData,1);
+		} else {
+			strDataRow = strDataRow + formatCol("ok",colData,1);
+		}
 	}
 	
 	
@@ -219,12 +283,38 @@ public class ReportMissingInformation extends Report {
 	// uses extraction from <name> rather than checking <SURN>
 	if(checkSurname) {
 		if(indi.getLastName() == "") {
-			strDataRow = strDataRow + align("X",colData,1);
-	} else {
-		strDataRow = strDataRow + align("ok",colData,1);
-	} 
-	} else {
-		strDataRow = strDataRow + align("-",colData,1);	
+			strDataRow = strDataRow + formatCol("X",colData,1);
+		} else {
+			strDataRow = strDataRow + formatCol("ok",colData,1);
+		} 
+	}
+
+	
+	//check occupation if required
+	if(checkOccupation) {		
+		// verify existence of occupation
+		if(indi.getProperty(new TagPath("INDI:OCCU")) == null) {		
+			strDataRow = strDataRow + formatCol("X",colData,1);
+		} else { 
+			strDataRow = strDataRow + formatCol("ok",colData,1);	
+		}
+	}
+
+	
+	//check occupation source if required
+	if(checkOccupationSource) {		
+		// verify existence of occupation
+		if(indi.getProperty(new TagPath("INDI:OCCU")) != null) {		
+			// verify existence and validity of source tag
+			tempSource = (PropertySource)indi.getProperty(new TagPath("INDI:OCCU:SOUR"));
+			if((tempSource == null) || (!tempSource.isValid())) {
+				strDataRow = strDataRow + formatCol("X",colData,1);
+			} else { 
+				strDataRow = strDataRow + formatCol("ok",colData,1);	
+			}
+		} else {
+			strDataRow = strDataRow + formatCol("-",colData,1);	
+		}
 	}
 	
 	//display results
@@ -266,11 +356,29 @@ public class ReportMissingInformation extends Report {
   public void displayHeader(String strSubject) {
   
 	  String strColHeader1, strColHeader2;
-	  String strUnderLine;
+	  String strUnderLine, strBlankNameHeader;
 	  int loop;	  
 	  
+	  //calculate number of verification columns
+	  numDataCols = 0;
+		if(checkIndividualSource)	numDataCols += 1;
+		if(checkBirthDate)	numDataCols += 1;
+		if(checkBirthPlace)	numDataCols += 1;
+		if(checkBirthSource)	numDataCols += 1;
+		if(checkBaptismDate)	numDataCols += 1;
+		if(checkBaptismPlace)	numDataCols += 1;
+		if(checkBaptismSource)	numDataCols += 1;
+		if(checkDeathDate)	numDataCols += 1;
+		if(checkDeathPlace)	numDataCols += 1;
+		if(checkDeathSource)	numDataCols += 1;
+		if(checkSex)	numDataCols += 1;
+		if(checkGiven)	numDataCols += 1;
+		if(checkSurname)	numDataCols += 1;
+		if(checkOccupation)	numDataCols += 1;
+		if(checkOccupationSource)	numDataCols += 1;
+	  
 	  //print report title
-	  println(align(textTitle, (colName + numDataCols*colData), 1));
+	  println(align(textTitle, (colId + colName + numDataCols*colData), 1));
 	  println();
 	  
 	  println(textSubject + ": " + strSubject);
@@ -278,35 +386,57 @@ public class ReportMissingInformation extends Report {
 	  println(textKey);
 	  println();
 	   
-	  strUnderLine = "-";
-	  for(loop=1; loop<(colName+numDataCols*colData)-1; loop++)
-		  strUnderLine += "-";
-	  
 	  //create column header labels
-	  strColHeader1 = align(" ", colName, 1)
-	  				+ align(textBirth, colData, 1)
-	  				+ align(textBirth, colData, 1)
-	  				+ align(textBaptism, colData, 1)
-	  				+ align(textBaptism, colData, 1)	  				
-	  				+ align(textDeath, colData, 1)
-	  				+ align(textDeath, colData, 1);
+	  strBlankNameHeader = " ";
+	  for(loop=1; loop<colName-1; loop++)
+		strBlankNameHeader += " ";
 	  
+	  strColHeader1 = "\t" + (formatForScreenDisplay ? strBlankNameHeader : ""); // Id Name
+		if(checkIndividualSource)	strColHeader1 += formatCol(textIndividual, colData, 1);
+		if(checkBirthDate)	strColHeader1 += formatCol(textBirth, colData, 1);
+		if(checkBirthPlace)	strColHeader1 += formatCol(textBirth, colData, 1);
+		if(checkBirthSource)	strColHeader1 += formatCol(textBirth, colData, 1);
+		if(checkBaptismDate)	strColHeader1 += formatCol(textBaptism, colData, 1);
+		if(checkBaptismPlace)	strColHeader1 += formatCol(textBaptism, colData, 1);
+		if(checkBaptismSource)	strColHeader1 += formatCol(textBaptism, colData, 1);
+		if(checkDeathDate)	strColHeader1 += formatCol(textDeath, colData, 1);
+		if(checkDeathPlace)	strColHeader1 += formatCol(textDeath, colData, 1);
+		if(checkDeathSource)	strColHeader1 += formatCol(textDeath, colData, 1);
+		if(checkSex)	strColHeader1 += formatCol("", colData, 1);
+		if(checkGiven)	strColHeader1 += formatCol("", colData, 1);
+		if(checkSurname)	strColHeader1 += formatCol("", colData, 1);
+		if(checkOccupation)	strColHeader1 += formatCol("", colData, 1);
+		if(checkOccupationSource)	strColHeader1 += formatCol(textOccupation, colData, 1);
 	  
-	  strColHeader2 = align(" ", colName, 1)
-	  				+ align(textDate, colData, 1)
-	  				+ align(textPlace, colData, 1)
-	    			+ align(textDate, colData, 1)
-	    			+ align(textPlace, colData, 1)
-	    			+ align(textDate, colData, 1)
-	    			+ align(textPlace, colData, 1)	    			
-	    			+ align(textSex,colData, 1) 
-	    			+ align(textGiven,colData,1) 
-	    			+ align(textSurname,colData,1);
+	  strColHeader2 = "Id\t" + (formatForScreenDisplay ? strBlankNameHeader : ""); // Id Name
+		if(checkIndividualSource)	strColHeader2 += formatCol(textSource, colData, 1);
+		if(checkBirthDate)	strColHeader2 += formatCol(textDate, colData, 1);
+		if(checkBirthPlace)	strColHeader2 += formatCol(textPlace, colData, 1);
+		if(checkBirthSource)	strColHeader2 += formatCol(textSource, colData, 1);
+		if(checkBaptismDate)	strColHeader2 += formatCol(textDate, colData, 1);
+		if(checkBaptismPlace)	strColHeader2 += formatCol(textPlace, colData, 1);
+		if(checkBaptismSource)	strColHeader2 += formatCol(textSource, colData, 1);
+		if(checkDeathDate)	strColHeader2 += formatCol(textDate, colData, 1);
+		if(checkDeathPlace)	strColHeader2 += formatCol(textPlace, colData, 1);
+		if(checkDeathSource)	strColHeader2 += formatCol(textSource, colData, 1);
+		if(checkSex)	strColHeader2 += formatCol(textSex, colData, 1);
+		if(checkGiven)	strColHeader2 += formatCol(textGiven, colData, 1);
+		if(checkSurname)	strColHeader2 += formatCol(textSurname, colData, 1);
+		if(checkOccupation)	strColHeader2 += formatCol(textOccupation, colData, 1);
+		if(checkOccupationSource)	strColHeader2 += formatCol(textSource, colData, 1);
  
 	  //display
 	  println(strColHeader1);
 	  println(strColHeader2);
-	  println(strUnderLine);
+
+	  if (formatForScreenDisplay) {
+		strUnderLine = "-";
+		for(loop=1; loop<(colId+colName+numDataCols*colData)-1; loop++)
+			strUnderLine += "-";
+		println(strUnderLine);
+	  } else {
+		println();
+	  }
   
   }
 } 
