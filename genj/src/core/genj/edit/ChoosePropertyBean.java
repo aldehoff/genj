c967751ed9fc03b9c9f28ac0a686e10cbfb5af76
nmeier <nmeier@fbd980a2-0dff-11df-b4b7-0b05b74d0985>
2012-10-25 18:42:51 +0000

    java 7 compile probs
    git-svn-id: https://svn.code.sf.net/p/genj/code/trunk@6757 fbd980a2-0dff-11df-b4b7-0b05b74d0985

--- genj/src/core/genj/edit/ChoosePropertyBean.java
+++ genj/src/core/genj/edit/ChoosePropertyBean.java
@@ -187,7 +187,7 @@ public class ChoosePropertyBean extends JComponent {
   /**
    * Tag List Cell Renderer
    */
-  class MetaDefRenderer extends DefaultListCellRenderer implements ListCellRenderer {
+  class MetaDefRenderer extends DefaultListCellRenderer {
 
     /**
      * Return component for rendering list element
